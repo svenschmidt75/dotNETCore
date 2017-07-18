b14f9b067a032bd23192d3b150ae6237d8bbc132
Sven Schmidt <svenschmidt75@googlemail.com>
2017-07-17 18:18:34 -0600

    add HasSet project

---
+++ DataStructures/HashSet/HashSet.UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace HashSet.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var hasSet = new HashSet<int>();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ DataStructures/HashSet/HashSet/HashSet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet
+{
+    public class HashSet<T> : ICollection<T>
+    {
+        private List<LinkedList<T>> _data = new List<LinkedList<T>>();
+
+    }
+}

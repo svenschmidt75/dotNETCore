3fa8e16efb0878bb98da87cdcb3d13f35749516e
Sven Schmidt <svenschmidt75@googlemail.com>
2017-07-17 18:55:21 -0600

    more HashSet code

--- DataStructures/HashSet/HashSet.UnitTest/UnitTest1.cs
+++ DataStructures/HashSet/HashSet.UnitTest/UnitTest1.cs
@@ -6,14 +6,47 @@ namespace HashSet.UnitTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Add_From_Empty()
         {
             // Arrange
-            var hasSet = new HashSet<int>();
+            var hashSet = new HashSet<int>();
 
             // Act
+            hashSet.Add(1);
 
             // Assert
+            Assert.True(hashSet.Contains(1));
+            Assert.Equal(1, hashSet.Count);
         }
+
+        [Fact]
+        public void Add_Unique()
+        {
+            // Arrange
+            var hashSet = new HashSet<int>();
+
+            // Act
+            hashSet.Add(1);
+            hashSet.Add(1);
+
+            // Assert
+            Assert.True(hashSet.Contains(1));
+        }
+
+        [Fact]
+        public void Remove()
+        {
+            // Arrange
+            var hashSet = new HashSet<int>();
+            hashSet.Add(1);
+
+            // Act
+            hashSet.Remove(1);
+
+            // Assert
+            Assert.False(hashSet.Contains(1));
+            Assert.Equal(0, hashSet.Count);
+        }
+
     }
 }
--- DataStructures/HashSet/HashSet/HashSet.cs
+++ DataStructures/HashSet/HashSet/HashSet.cs
@@ -1,11 +1,85 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace HashSet
 {
     public class HashSet<T> : ICollection<T>
     {
-        private List<LinkedList<T>> _data = new List<LinkedList<T>>();
+        public delegate int Hash(T value);
 
+        private const int Size = 1876;
+        private readonly Hash _hash;
+        private readonly LinkedList<T>[] _data = new LinkedList<T>[Size];
+
+        public HashSet() : this(value => value.GetHashCode())
+        {
+        }
+
+        public HashSet(Hash hash)
+        {
+            _hash = hash;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _data.Where(x => x != null).SelectMany(bucket => bucket).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private int GetHash(T item)
+        {
+            var index = _hash(item) % Size;
+            if (index < 0 || index >= Size)
+            {
+                throw new InvalidOperationException("Hash index too large");
+            }
+            return index;
+        }
+
+        public void Add(T item)
+        {
+            var index = GetHash(item);
+            if (Exists(item, index))
+                return;
+            var bucket = new LinkedList<T>();
+            bucket.AddFirst(item);
+            _data[index] = bucket;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            return Exists(item, GetHash(item));
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count => _data.Where(x => x != null).SelectMany(bucket => bucket).Count();
+        public bool IsReadOnly { get; }
+
+        private bool Exists(T item, int index)
+        {
+            var bucket = _data[index];
+            return bucket != null && bucket.Any(x => x.Equals(item));
+        }
     }
 }

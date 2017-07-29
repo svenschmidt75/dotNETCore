388ae83b1d6b3b21ba61e99a1bb77b3dd6291d35
Sven Schmidt <svenschmidt75@googlemail.com>
2017-07-29 10:51:58 -0600

    Djikstra

--- DataStructures/Djikstra/Djikstra.UnitTest/DjikstraTest.cs
+++ DataStructures/Djikstra/Djikstra.UnitTest/DjikstraTest.cs
@@ -64,5 +64,39 @@ namespace Djikstra.UnitTest
             Assert.Equal(new[] {startNode, bNode, cNode, dNode, endNode}, shortestPath);
         }
 
+        [Fact]
+        public void GrokkinAlgortihms_Page_125()
+        {
+            // Arrange
+            var startNode = new Node("Book - Start");
+            var aNode = new Node("LP");
+            var bNode = new Node("Poster");
+            var cNode = new Node("Guitar");
+            var dNode = new Node("Drum");
+            var endNode = new Node("Piano - End");
+
+            var graph = new Graph(startNode, endNode);
+            var edges = graph.Add(startNode);
+            edges.Add(new Edge {Node = aNode, Weight = 5});
+            edges.Add(new Edge {Node = bNode, Weight = 0});
+            edges = graph.Add(aNode);
+            edges.Add(new Edge {Node = cNode, Weight = 15});
+            edges.Add(new Edge {Node = dNode, Weight = 20});
+            edges = graph.Add(bNode);
+            edges.Add(new Edge {Node = cNode, Weight = 30});
+            edges.Add(new Edge {Node = dNode, Weight = 35});
+            edges = graph.Add(cNode);
+            edges.Add(new Edge {Node = endNode, Weight = 20});
+            edges = graph.Add(dNode);
+            edges.Add(new Edge {Node = endNode, Weight = 10});
+            graph.Add(endNode);
+
+            // Act
+            var shortestPath = Djikstra.Run(graph);
+
+            // Assert
+            Assert.Equal(new[] {startNode, aNode, dNode, endNode}, shortestPath);
+        }
+
     }
 }

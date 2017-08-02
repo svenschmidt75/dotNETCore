741d482811ea1ee33a82d00f0c4d52668e1dbd46
Sven Schmidt <svenschmidt75@googlemail.com>
2017-08-01 19:16:46 -0600

    qsort

---
+++ DataStructures/QuickSort/QuickSort.UnitTest/QuickSortTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace QuickSort.UnitTest
+{
+    public class QuickSortTest
+    {
+        private static bool IsSorted<T>(T[] sorted) where T : IComparable<T>
+        {
+            return sorted.Zip(sorted.Skip(1), (arg1, arg2) => new {Item1 = arg1, Item2 = arg2}).
+                          All(arg => arg.Item1.CompareTo(arg.Item2) < 1);
+        }
+
+        [Fact]
+        public void Test_Empty()
+        {
+            // Arrange
+            var a = new int[0];
+
+            // Act
+            QuickSort.Sort(a);
+
+            // Assert
+            Assert.True(IsSorted(a));
+        }
+
+        [Fact]
+        public void Test_Single()
+        {
+            // Arrange
+            var a = new[] {1};
+
+            // Act
+            QuickSort.Sort(a);
+
+            // Assert
+            Assert.True(IsSorted(a));
+        }
+
+        [Fact]
+        public void Test_Two()
+        {
+            // Arrange
+            var a = new[] {1, 0};
+
+            // Act
+            QuickSort.Sort(a);
+
+            // Assert
+            Assert.True(IsSorted(a));
+        }
+
+    }
+}
---
+++ DataStructures/QuickSort/QuickSort/QuickSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace QuickSort
+{
+    public static class QuickSort
+    {
+        public static void Sort<T>(T[] input) where T: IComparable<T>
+        {
+            Sort(input, 0, input.Length - 1);
+        }
+
+        private static void Sort<T>(T[] input, int startIndex, int endIndex) where T: IComparable<T>
+        {
+            if (input.Length < 2)
+            {
+                return;
+            }
+            if (input.Length == 2)
+            {
+                if (input[0].CompareTo(input[1]) == 1)
+                {
+                    T tmp = input[1];
+                    input[0] = input[1];
+                    input[1] = tmp;
+                    return;
+                }
+            }
+
+            // select a pivot element
+            T pivot = input[0];
+
+            // partition the array
+            var partitioned = new T[endIndex - startIndex + 1];
+
+            int j = 0;
+            int pivotIndex = 0;
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                var elem = input[i];
+                if (elem.CompareTo(pivot) < 1)
+                {
+                    partitioned[j++] = elem;
+                }
+            }
+            pivotIndex = j - 1;
+            // TODO SS: do the ones larger and start from the end backwards...
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                var elem = input[i];
+                if (elem.CompareTo(pivot) == 1)
+                {
+                    partitioned[j++] = elem;
+                }
+            }
+            Sort(input, startIndex, pivotIndex);
+            Sort(input, pivotIndex + 1, endIndex);
+        }
+
+    }
+}

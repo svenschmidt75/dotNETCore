c6945e7a68413eae066484a151ee20faacd6e988
Sven Schmidt <svenschmidt75@googlemail.com>
2017-08-05 09:13:20 -0600

    creating the power set works

--- DataStructures/RadioStationsSetCovering/RadioStationsSetCovering.UnitTest/RadioStationsSetCoveringTests.cs
+++ DataStructures/RadioStationsSetCovering/RadioStationsSetCovering.UnitTest/RadioStationsSetCoveringTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace RadioStationsSetCovering.UnitTest
@@ -7,7 +8,93 @@ namespace RadioStationsSetCovering.UnitTest
     public class RadioStationsSetCoveringTests
     {
         [Fact]
-        public void Test1()
+        public void EmptySet()
+        {
+            // Arrange
+            var set = new Dictionary<int, HashSet<int>>();
+            var universe = new HashSet<int> {1, 2, 3, 4, 5};
+
+            // Act
+            var covering = SetCovering.Run(set, universe);
+
+            // Assert
+            Assert.Equal(covering, new List<HashSet<int>> {SetCovering.EmptySet});
+        }
+
+        [Fact]
+        public void SingleSet()
+        {
+            // Arrange
+            var set = new Dictionary<int, HashSet<int>>
+            {
+                {0, new HashSet<int> {1, 2, 3}},
+            };
+            var universe = new HashSet<int> {1, 2, 3, 4, 5};
+
+            // Act
+            var covering = SetCovering.Run(set, universe);
+
+            // Assert
+            Assert.Equal(covering, new List<HashSet<int>> {SetCovering.EmptySet, new HashSet<int> {0}});
+        }
+
+        [Fact]
+        public void SetWith2Items()
+        {
+            // Arrange
+            var set = new Dictionary<int, HashSet<int>>
+            {
+                {0, new HashSet<int> {1, 2, 3}},
+                {3, new HashSet<int> {4, 5}},
+            };
+            var universe = new HashSet<int> {1, 2, 3, 4, 5};
+
+            // Act
+            var covering = SetCovering.Run(set, universe);
+
+            // Assert
+            Assert.Equal(covering,
+                         new List<HashSet<int>>
+                         {
+                             SetCovering.EmptySet,
+                             new HashSet<int> {0},
+                             new HashSet<int> {3},
+                             new HashSet<int> {0, 3}
+                         });
+        }
+
+        [Fact]
+        public void SetWith3Items()
+        {
+            // Arrange
+            var set = new Dictionary<int, HashSet<int>>
+            {
+                {0, new HashSet<int> {1, 2, 3}},
+                {1, new HashSet<int> {2, 4}},
+                {3, new HashSet<int> {4, 5}},
+            };
+            var universe = new HashSet<int> {1, 2, 3, 4, 5};
+
+            // Act
+            var covering = SetCovering.Run(set, universe);
+
+            // Assert
+            Assert.Equal(covering,
+                         new List<HashSet<int>>
+                         {
+                             SetCovering.EmptySet,
+                             new HashSet<int> {0},
+                             new HashSet<int> {1},
+                             new HashSet<int> {0, 1},
+                             new HashSet<int> {3},
+                             new HashSet<int> {0, 3},
+                             new HashSet<int> {1, 3},
+                             new HashSet<int> {0, 1, 3}
+                         });
+        }
+
+        [Fact]
+        public void SetWith4Items()
         {
             // Arrange
             var set = new Dictionary<int, HashSet<int>>
--- DataStructures/RadioStationsSetCovering/RadioStationsSetCovering/SetCovering.cs
+++ DataStructures/RadioStationsSetCovering/RadioStationsSetCovering/SetCovering.cs
@@ -7,31 +7,39 @@ namespace RadioStationsSetCovering
 {
     public static class SetCovering
     {
+        public static HashSet<int> EmptySet => new HashSet<int>();
+
         /// <summary>
-        /// The set cover problem is to identify the smallest sub-collection of
-        /// S whose union equals the universe.
+        ///     The set cover problem is to identify the smallest sub-collection of
+        ///     S whose union equals the universe.
         /// </summary>
-        public static IEnumerable<List<T>> Run<T>(IDictionary<int, HashSet<T>> set, HashSet<T> universe)
+        public static IEnumerable<HashSet<T>> Run<T>(IDictionary<int, HashSet<T>> set, HashSet<T> universe)
         {
-            return Enumerable.Empty<List<T>>();
+            var powerSet = CreatePowerSet(set);
+            
+
+
+
+
+            return (IEnumerable<HashSet<T>>) CreatePowerSet(set);
         }
 
         /// <summary>
-        /// The power set, i.e. the combinations of a set with all other sets,
-        /// is stored by radio station index, rather than doing the unions
-        /// explicitly.
+        ///     The power set, i.e. the combinations of a set with all other sets,
+        ///     is stored by radio station index, rather than doing the unions
+        ///     explicitly.
         /// </summary>
         public static List<HashSet<int>> CreatePowerSet<T>(IDictionary<int, HashSet<T>> set)
         {
-            int nPowerSet = (int) Math.Pow(2, set.Count());
+            var nPowerSet = (int) Math.Pow(2, set.Count());
             var powerSet = new List<HashSet<int>>(nPowerSet);
 
             // add the empty set, so each set is added on its own
-            powerSet.Add(new HashSet<int>());
+            powerSet.Add(EmptySet);
 
             set.ForEach(item =>
             {
-                int id = item.Key;
+                var id = item.Key;
 
                 var c = new List<HashSet<int>>();
 
@@ -40,10 +48,13 @@ namespace RadioStationsSetCovering
                 {
                     // combine the two sets items and pi
                     var combined = new HashSet<int>();
-                    combined.Union(pi);
+                    combined.UnionWith(pi);
                     combined.Add(id);
                     c.Add(combined);
                 });
+
+                // add all combination with item to the power set
+                powerSet.AddRange(c);
             });
 
             return powerSet;

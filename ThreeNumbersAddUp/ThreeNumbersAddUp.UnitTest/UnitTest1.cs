ae5e21f70c1c8c1e840c3ca129e9b3e3bba5b4b9
Sven Schmidt <svenschmidt75@googlemail.com>
2017-09-01 20:41:19 -0600

    a1 + a2 + a3 = a

---
+++ ThreeNumbersAddUp/ThreeNumbersAddUp.UnitTest/UnitTest1.cs
@@ -0,0 +1,93 @@
+using System.Linq;
+using Xunit;
+
+namespace ThreeNumbersAddUp.UnitTest
+{
+    public class UnitTest1
+    {
+        // Test5: a1 + a2 + a3 > value
+        // Test6: a1 + a2 + a3 = value
+        // Test7: a1 + a2 + a3 < value
+
+        [Fact]
+        public void a1_bigger_than_value()
+        {
+            // Arrange
+            var numbers = new[] {1, 2, 5, 6};
+
+            // Act
+            var solution = Class1.ThreeNumbers(numbers, 4);
+
+            // Assert
+            Assert.False(solution.Any());
+        }
+
+        [Fact]
+        public void a1_equals_value()
+        {
+            // Arrange
+            var numbers = new[] {1, 2, 5, 6};
+
+            // Act
+            var solution = Class1.ThreeNumbers(numbers, 2);
+
+            // Assert
+            Assert.False(solution.Any());
+        }
+
+        [Fact]
+        public void a1_plus_a2_bigger_than_value()
+        {
+            // Arrange
+            var numbers = new[] {1, 2, 5, 6};
+
+            // Act
+            var solution = Class1.ThreeNumbers(numbers, 4);
+
+            // Assert
+            Assert.False(solution.Any());
+        }
+
+        [Fact]
+        public void a1_plus_a2_equals_value()
+        {
+            // Arrange
+            var numbers = new[] {1, 2, 5, 6};
+
+            // Act
+            var solution = Class1.ThreeNumbers(numbers, 3);
+
+            // Assert
+            Assert.False(solution.Any());
+        }
+
+        [Fact]
+        public void a1_plus_a2_plus_a3_bigger_than_value()
+        {
+            // Arrange
+            var numbers = new[] {1, 2, 5, 6};
+
+            // Act
+            var solution = Class1.ThreeNumbers(numbers, 7);
+
+            // Assert
+            Assert.False(solution.Any());
+        }
+
+        [Theory]
+        [InlineData(8, new[] {1, 2, 5})]
+        [InlineData(13, new[] {2, 5, 6})]
+        public void a1_plus_a2_plus_a3_equals_value(int value, int[] vs)
+        {
+            // Arrange
+            var numbers = new[] {1, 2, 5, 6};
+
+            // Act
+            var solution = Class1.ThreeNumbers(numbers, value);
+
+            // Assert
+            Assert.Equal(3, solution.Count());
+            Assert.Equal(solution, vs);
+        }
+    }
+}
---
+++ ThreeNumbersAddUp/ThreeNumbersAddUp/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThreeNumbersAddUp
+{
+    public static class Class1
+    {
+        public static IEnumerable<int> ThreeNumbers(IList<int> numbers, int value)
+        {
+            for (int i = 0; i < numbers.Count - 2; i++)
+            {
+                int a1 = numbers[i];
+                if (a1 > value)
+                    continue;
+                for (int j = i + 1; j < numbers.Count - 1; j++)
+                {
+                    int a2 = numbers[j];
+                    if (a1 + a2 > value)
+                        continue;
+                    for (int k = j + 1; k < numbers.Count; k++)
+                    {
+                        int a3 = numbers[k];
+                        if (a1 + a2 + a3 > value)
+                            continue;
+                        if (a1 + a2 + a3 == value)
+                        {
+                            return new[] {a1, a2, a3};
+                        }
+                    }
+                }
+            }
+            return Enumerable.Empty<int>();
+        }
+    }
+}

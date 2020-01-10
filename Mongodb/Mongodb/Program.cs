ab1984db03431f4ee351048d4fb3cd70faead449
Sven <svenschmidt75@googlemail.com>
2020-01-09 17:36:15 -0700

    simple code to access MongoDB

---
+++ Mongodb/Mongodb/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace Mongodb
+{
+    public class Unit
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("TestDatabase");
+            var data = database.GetCollection<Unit>("Units");
+            var units = data.Find(new BsonDocument()).ToList();
+
+            Console.WriteLine();
+        }
+    }
+}

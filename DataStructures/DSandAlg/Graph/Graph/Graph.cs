86be33cfd1bb6755e199059e675d88d41ec9f18c
Sven <svenschmidt75@googlemail.com>
2020-01-14 17:21:43 -0700

    add graph data structure

---
+++ DataStructures/DSandAlg/Graph/Graph/Graph.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Graph
+{
+    public class Graph
+    {
+        private readonly Dictionary<int, List<(int, int)>> _adjacencyList = new Dictionary<int, List<(int, int)>>();
+        private int _nNodes = 0;
+
+        public void AddNode(int node)
+        {
+            _adjacencyList[node] = new List<(int, int)>();
+            _nNodes++;
+        }
+
+        public void AddUndirectedEdge(int fromNode, int toNode, int weight = 0)
+        {
+            AddEdge(fromNode, toNode, weight);
+            AddEdge(toNode, fromNode, weight);
+        }
+
+        private void AddEdge(int fromNode, int toNode, int weight)
+        {
+            _adjacencyList[fromNode].Add((toNode, weight));
+        }
+
+        public void AddDirectedEdge(int fromNode, int toNode, int weight)
+        {
+            AddEdge(fromNode, toNode, weight);
+        }
+    }
+}

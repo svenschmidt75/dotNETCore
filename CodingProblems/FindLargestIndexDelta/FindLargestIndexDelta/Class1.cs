682436816953d35be01e502f482c5ffe36c4a412
Sven <svenschmidt75@googlemail.com>
2020-06-03 17:28:50 -0600

    sort log file lines

--- CodingProblems/FindLargestIndexDelta/FindLargestIndexDelta/Class1.cs
+++ CodingProblems/FindLargestIndexDelta/FindLargestIndexDelta/Class1.cs
@@ -57,13 +57,57 @@ namespace FindLargestIndexDelta
 
             return maxDiff;
         }
+
+        public static int Solve2(int[] array)
+        {
+            // SS: O(n log n)
+
+            // SS: construct array with values such that at each index, we find
+            // the smallest value
+            var minArray = new int[array.Length];
+            minArray[0] = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                minArray[i] = Math.Min(array[i], minArray[i - 1]);
+            }
+
+            // SS: construct array with values such that at each index, we find
+            // the largest value
+            var maxArray = new int[array.Length];
+            minArray[^1] = array[^1];
+            for (int i = array.Length - 2; i >= 0; i--)
+            {
+                maxArray[i] = Math.Max(array[i], maxArray[i + 1]);
+            }
+
+            // SS: example: minArray[5]: min element up to index 5
+
+            int lIdx = 0;
+            int rIdx = array.Length - 1;
+            int maxDiff = 0;
+            while (lIdx < array.Length - 1 && rIdx < array.Length - 1)
+            {
+                if (minArray[lIdx] < maxArray[rIdx])
+                {
+                    maxDiff = Math.Max(maxDiff, rIdx - lIdx);
+                    rIdx -= 1;
+                }
+                else
+                {
+                    lIdx += 1;
+                }
+            }
+
+            return maxDiff;
+        }
     }
+    
 
     [TestFixture]
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void Test11()
         {
             // Arrange
             var array = new[] {34, 8, 10, 3, 2, 80, 30, 33, 1};
@@ -76,7 +120,7 @@ namespace FindLargestIndexDelta
         }
 
         [Test]
-        public void Test2()
+        public void Test12()
         {
             // Arrange
             var array = new[] {9, 2, 3, 4, 5, 6, 7, 8, 18, 0};
@@ -87,5 +131,32 @@ namespace FindLargestIndexDelta
             // Assert
             Assert.AreEqual(8, maxDiff);
         }
+        
+        [Test]
+        public void Test21()
+        {
+            // Arrange
+            var array = new[] {34, 8, 10, 3, 2, 80, 30, 33, 1};
+
+            // Act
+            var maxDiff = Class1.Solve2(array);
+
+            // Assert
+            Assert.AreEqual(6, maxDiff);
+        }
+
+        [Test]
+        public void Test22()
+        {
+            // Arrange
+            var array = new[] {9, 2, 3, 4, 5, 6, 7, 8, 18, 0};
+
+            // Act
+            var maxDiff = Class1.Solve2(array);
+
+            // Assert
+            Assert.AreEqual(8, maxDiff);
+        }
+
     }
 }

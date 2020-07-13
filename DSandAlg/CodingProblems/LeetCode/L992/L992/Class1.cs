32bda72b49a91007ded6f6c278407cf6d4b5221b
Sven <svenschmidt75@googlemail.com>
2020-07-13 08:32:58 -0600

    LeetCode 992: fix some test cases, now accepted as solution

--- DSandAlg/CodingProblems/LeetCode/L992/L992/Class1.cs
+++ DSandAlg/CodingProblems/LeetCode/L992/L992/Class1.cs
@@ -86,6 +86,11 @@ namespace L992
                 {
                     if (jkMap.Count < K)
                     {
+                        if (j >= k)
+                        {
+                            break;
+                        }
+
                         var number = A[j];
                         AddElement(number, jkMap);
                         j++;
@@ -255,6 +260,40 @@ namespace L992
 
             // Assert
             Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            // Arrange
+            int[] A = {2, 2, 1, 2, 2, 2, 1, 1};
+            var k = 2;
+
+            // Act
+            var result = new Solution().SubarraysWithKDistinct(A, k);
+
+            // Assert
+            Assert.AreEqual(23, result);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            // Arrange
+            int[] A =
+            {
+                27, 27, 43, 28, 11, 20, 1, 4, 49, 18, 37, 31, 31, 7, 3, 31, 50, 6, 50, 46, 4, 13, 31, 49, 15, 52, 25, 31
+                , 35, 4, 11, 50, 40, 1, 49, 14, 46, 16, 11, 16, 39, 26, 13, 4, 37, 39, 46, 27, 49, 39, 49, 50, 37, 9, 30
+                , 45, 51, 47, 18, 49, 24, 24, 46, 47, 18, 46, 52, 47, 50, 4, 39, 22, 50, 40, 3, 52, 24, 50, 38, 30, 14
+                , 12, 1, 5, 52, 44, 3, 49, 45, 37, 40, 35, 50, 50, 23, 32, 1, 2
+            };
+            var k = 20;
+
+            // Act
+            var result = new Solution().SubarraysWithKDistinct(A, k);
+
+            // Assert
+            Assert.AreEqual(149, result);
         }
     }
 }

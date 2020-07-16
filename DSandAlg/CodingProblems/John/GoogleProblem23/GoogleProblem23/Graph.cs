ffa604108d39d5f8e5b5b2d1807ccc9f38c61b72
Sven <svenschmidt75@googlemail.com>
2020-07-16 16:34:52 -0600

    add DFS to solve for more general case

---
+++ DSandAlg/CodingProblems/John/GoogleProblem23/GoogleProblem23/Graph.cs
@@ -0,0 +1,34 @@
+﻿#region
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace GoogleProblem23
+{
+    public class Graph
+    {
+        public Dictionary<int, List<int>> AdjacencyList { get; } = new Dictionary<int, List<int>>();
+
+        public void AddVertex(int node)
+        {
+            AdjacencyList[node] = new List<int>();
+        }
+
+        public void AddUndirectedEdge(int fromNode, int toNode)
+        {
+            AddEdge(fromNode, toNode);
+            AddEdge(toNode, fromNode);
+        }
+
+        private void AddEdge(int fromNode, int toNode)
+        {
+            AdjacencyList[fromNode].Add(toNode);
+        }
+
+        public void AddDirectedEdge(int fromNode, int toNode)
+        {
+            AddEdge(fromNode, toNode);
+        }
+    }
+}

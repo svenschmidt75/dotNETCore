d7d8897a2390436f85f90b5b82121fc7999ed93f
Sven Schmidt <s.schmidt@questintegrity.com>
2020-08-11 15:05:12 -0600

    minor changes

--- DSandAlg/CodingProblems/John/GoogleProblem28/GoogleProblem28/Solution.cs
+++ DSandAlg/CodingProblems/John/GoogleProblem28/GoogleProblem28/Solution.cs
@@ -9,39 +9,35 @@ namespace GoogleProblem28
 {
     public class Solution
     {
-        public int Solve(int startLocation, int[][] holidayByLocation, int[][] flightDuration)
+        public int Solve(int startLocation, int[][] holidayByLocation, int[][] flightDurations)
         {
             var graph = new Graph();
 
             // SS: for each location, add a vertex to the graph
-            for (var i = 0; i < flightDuration.Length; i++)
+            for (var i = 0; i < flightDurations.Length; i++)
             {
                 graph.AddVertex(i);
             }
 
-            for (var i = 0; i < flightDuration.Length; i++)
+            for (var i = 0; i < flightDurations.Length; i++)
             {
-                var destinations = flightDuration[i];
+                var destinations = flightDurations[i];
 
                 for (var j = 0; j < destinations.Length; j++)
                 {
-                    var flightDur = destinations[j];
-
-                    if (flightDur <= 6)
+                    var flightDuration = destinations[j];
+                    if (flightDuration <= 6)
                     {
                         graph.AddUndirectedEdge(i, j);
                     }
                 }
             }
 
-            var itinarary = MaximizeNationalHolidays(graph, startLocation, 0, new List<int> {startLocation}, 0
-                , holidayByLocation);
-
+            var itinarary = MaximizeNationalHolidays(graph, startLocation, 0, new List<int> {startLocation}, holidayByLocation);
             return itinarary;
         }
 
-        private int MaximizeNationalHolidays(Graph graph, int currentLocation, int month, List<int> path, int nHolidays
-            , int[][] holidayByLocation)
+        private int MaximizeNationalHolidays(Graph graph, int currentLocation, int month, List<int> path, int[][] holidayByLocation)
         {
             // SS: divide and conquer approach
 
@@ -54,29 +50,25 @@ namespace GoogleProblem28
             // SS: number of holidays for this month at the current location
             var n = holidayByLocation[currentLocation][month];
 
-            var bestLocationHolidays = -1;
-            var bestLocation = -1;
+            var bestLocationHolidays = n;
+            var bestLocation = currentLocation;
 
             // SS: find all locations that we can reach from this one (this includes the current one)
             var targetLocations = graph.AdjacencyList[currentLocation];
             for (var i = 0; i < targetLocations.Count; i++)
             {
                 var targetLocation = targetLocations[i];
-                var c = MaximizeNationalHolidays(graph, targetLocation, month + 1, path, nHolidays, holidayByLocation);
-                if (bestLocation == -1 || c > bestLocation)
+                var c = MaximizeNationalHolidays(graph, targetLocation, month + 1, path, holidayByLocation);
+                if (c + n > bestLocationHolidays)
                 {
-                    bestLocationHolidays = c;
+                    bestLocationHolidays = c + n;
                     bestLocation = targetLocation;
                 }
             }
 
-            if (bestLocation > -1)
-            {
-                path.Add(bestLocation);
-                return n + bestLocationHolidays;
-            }
+//            path.Add(bestLocation);
 
-            return nHolidays;
+            return bestLocationHolidays;
         }
 
         [TestFixture]
@@ -88,21 +80,21 @@ namespace GoogleProblem28
                 // Arrange
                 var holidaysByLocation = new[]
                 {
-                    new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                    new[]{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                    , new[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}
+                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
                     , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0}
                 };
 
                 // SS: flight durations don't have to be symmetric between locations...
                 int[][] flightDuration = new[]
                 {
-                    new[]{0, 2, 7, 9, 3}
-                    , new[]{2, 0, 7, 9, 3}
-                    , new[]{2, 7, 0, 9, 3}
-                    , new[]{2, 7, 9, 0, 3}
-                    , new[]{2, 9, 9, 3, 0}
+                    new[]{0, 2, 7, 7, 7}
+                    , new[]{2, 0, 2, 3, 7}
+                    , new[]{7, 2, 0, 2, 7}
+                    , new[]{7, 2, 3, 0, 3}
+                    , new[]{7, 7, 7, 2, 0}
                 };
 
                 // Act

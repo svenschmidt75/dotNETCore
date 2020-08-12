e60e2afe73dbb1ca24622b1929697e47ac5aa6ab
Sven Schmidt <svenschmidt75@googlemail.com>
2020-08-12 09:01:40 -0600

    add top down DP solution

--- DSandAlg/CodingProblems/John/GoogleProblem28/GoogleProblem28/Solution.cs
+++ DSandAlg/CodingProblems/John/GoogleProblem28/GoogleProblem28/Solution.cs
@@ -1,6 +1,6 @@
 ﻿#region
 
-using System.Collections.Generic;
+using System;
 using NUnit.Framework;
 
 #endregion
@@ -9,7 +9,24 @@ namespace GoogleProblem28
 {
     public class Solution
     {
-        public int Solve(int startLocation, int[][] holidayByLocation, int[][] flightDurations)
+        public int SolveTopDownDP(int startLocation, int[][] holidayByLocation, int[][] flightDurations)
+        {
+            var graph = CreateGraph(flightDurations);
+
+            // SS: memoization array
+            // space complexity: O(#locations^2 * 12), i.e. each cell in the #locations * 12 grid stores
+            // the path of size #locations
+            var memArray = new Payload[holidayByLocation.Length][];
+            for (var i = 0; i < memArray.Length; i++)
+            {
+                memArray[i] = new Payload[holidayByLocation[i].Length];
+            }
+
+            var itinarary = MaximizeNationalHolidaysTopDownDP(graph, startLocation, 0, holidayByLocation, memArray);
+            return itinarary.Holidays;
+        }
+
+        private static Graph CreateGraph(int[][] flightDurations)
         {
             var graph = new Graph();
 
@@ -33,15 +50,15 @@ namespace GoogleProblem28
                 }
             }
 
-            var itinarary = MaximizeNationalHolidays(graph, startLocation, 0, new List<int> {startLocation}, holidayByLocation);
-            return itinarary;
+            return graph;
         }
 
-        private int MaximizeNationalHolidays(Graph graph, int currentLocation, int month, List<int> path, int[][] holidayByLocation)
+        private int MaximizeNationalHolidays(Graph graph, int currentLocation, int month, int[][] holidayByLocation)
         {
             // SS: divide and conquer approach
+            // runtime complexity: O(#locations^12), i.e. exponential complexity
 
-            if (month == 4)
+            if (month == 12)
             {
                 // done
                 return 0;
@@ -51,24 +68,73 @@ namespace GoogleProblem28
             var n = holidayByLocation[currentLocation][month];
 
             var bestLocationHolidays = 0;
-            var bestLocation = currentLocation;
 
             // SS: find all locations that we can reach from this one (this includes the current one)
             var targetLocations = graph.AdjacencyList[currentLocation];
             for (var i = 0; i < targetLocations.Count; i++)
             {
                 var targetLocation = targetLocations[i];
-                var c = MaximizeNationalHolidays(graph, targetLocation, month + 1, path, holidayByLocation);
+                var c = MaximizeNationalHolidays(graph, targetLocation, month + 1, holidayByLocation);
                 if (c >= bestLocationHolidays)
                 {
                     bestLocationHolidays = c;
-                    bestLocation = targetLocation;
                 }
             }
 
-//            path.Add(bestLocation);
-
             return n + bestLocationHolidays;
+        }
+
+        private Payload MaximizeNationalHolidaysTopDownDP(Graph graph, int currentLocation, int month, int[][] holidayByLocation, Payload[][] memArray)
+        {
+            // SS: top-down dynamic programming solution
+            // runtime complexity: O(#locations * 12), i.e. O(#locations)
+
+            const int maxMonths = 12;
+            if (month == maxMonths)
+            {
+                // done
+                return null;
+            }
+
+            var payload = memArray[currentLocation][month];
+            if (payload != null)
+            {
+                return payload;
+            }
+
+            // SS: number of holidays for this month at the current location
+            var n = holidayByLocation[currentLocation][month];
+
+            var bestPayload = new Payload {Holidays = 0, Path = new int[maxMonths]};
+
+            // SS: find all locations that we can reach from this one
+            // (this includes the current one, i.e. we stay at the location we are)
+            var targetLocations = graph.AdjacencyList[currentLocation];
+            for (var i = 0; i < targetLocations.Count; i++)
+            {
+                var targetLocation = targetLocations[i];
+                var c = MaximizeNationalHolidaysTopDownDP(graph, targetLocation, month + 1, holidayByLocation, memArray);
+                if (c != null && c.Holidays >= bestPayload.Holidays)
+                {
+                    bestPayload = c;
+                }
+            }
+
+            // SS: create copy of path
+            var p = new int[maxMonths];
+            Array.Copy(bestPayload.Path, p, maxMonths);
+            p[month] = currentLocation + 1;
+
+            payload = new Payload {Holidays = n + bestPayload.Holidays, Path = p};
+            memArray[currentLocation][month] = payload;
+
+            return payload;
+        }
+
+        private class Payload
+        {
+            public int Holidays { get; set; } = -1;
+            public int[] Path { get; set; } = new int[12];
         }
 
         [TestFixture]
@@ -78,30 +144,31 @@ namespace GoogleProblem28
             public void Test1()
             {
                 // Arrange
-                
+
                 // row: location
                 // column: month
                 var holidaysByLocation = new[]
                 {
-                    new[]{0, 1, 0, 0}
-                    , new[]{0, 1, 0, 0}
-                    , new[]{0, 0, 1, 0}
-                    , new[]{0, 0, 0, 1}
+                    new[] {0, 0, 0, 0}
+                    , new[] {0, 1, 0, 0}
+                    , new[] {0, 0, 1, 0}
+                    , new[] {0, 0, 0, 1}
                 };
 
                 // SS: flight durations don't have to be symmetric between locations...
-                int[][] flightDuration = new[]
+                int[][] flightDuration =
                 {
-                    new[]{0, 2, 7, 7}
-                    , new[]{2, 0, 2, 7}
-                    , new[]{7, 2, 0, 2}
-                    , new[]{7, 7, 7, 0}
+                    new[] {0, 2, 7, 7}
+                    , new[] {2, 0, 2, 7}
+                    , new[] {7, 2, 0, 2}
+                    , new[] {7, 7, 7, 0}
                 };
 
                 // Act
-                var itinerary = new Solution().Solve(0, holidaysByLocation, flightDuration);
+                var itinerary = new Solution().SolveTopDownDP(0, holidaysByLocation, flightDuration);
 
                 // Assert
+                Assert.AreEqual(3, itinerary);
             }
 
             [Test]
@@ -110,27 +177,28 @@ namespace GoogleProblem28
                 // Arrange
                 var holidaysByLocation = new[]
                 {
-                    new[]{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-                    , new[]{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}
-                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
-                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-                    , new[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0}
+                    new[] {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                    , new[] {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}
+                    , new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+                    , new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                    , new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0}
                 };
 
                 // SS: flight durations don't have to be symmetric between locations...
-                int[][] flightDuration = new[]
+                int[][] flightDuration =
                 {
-                    new[]{0, 2, 7, 7, 7}
-                    , new[]{2, 0, 2, 3, 7}
-                    , new[]{7, 2, 0, 2, 7}
-                    , new[]{7, 2, 3, 0, 3}
-                    , new[]{7, 7, 7, 2, 0}
+                    new[] {0, 2, 7, 7, 7}
+                    , new[] {2, 0, 2, 3, 7}
+                    , new[] {7, 2, 0, 2, 7}
+                    , new[] {7, 2, 3, 0, 3}
+                    , new[] {7, 7, 7, 2, 0}
                 };
 
                 // Act
-                var itinerary = new Solution().Solve(0, holidaysByLocation, flightDuration);
+                var itinerary = new Solution().SolveTopDownDP(0, holidaysByLocation, flightDuration);
 
                 // Assert
+                Assert.AreEqual(4, itinerary);
             }
         }
     }

18c39549ccda1bb19f477b53dbcea72917b4fc10
Sven <svenschmidt75@googlemail.com>
2020-09-28 17:03:36 -0600

    primitive Minesweeper

--- DSandAlg/CodingProblems/John/GoogleProblem33/GoogleProblem33/Minesweeper.cs
+++ DSandAlg/CodingProblems/John/GoogleProblem33/GoogleProblem33/Minesweeper.cs
@@ -15,35 +15,41 @@ namespace GoogleProblem33
         private readonly int _ncols;
         private readonly int _nrows;
         private readonly int[][][] _grid;
+        private int nRevealedCells;
+        private int _nBombs;
 
         public Minesweeper(int nrows, int ncols, int nBombs, IBoardInitializer boardInitializer)
         {
             _nrows = nrows;
             _ncols = ncols;
+            _nBombs = nBombs;
+            
             // SS: initialize grid/game board
             _grid = new int[nrows][][];
 
             for (var i = 0; i < nrows; i++)
             {
-                _grid[i] = new int[ncols][];
+                Grid[i] = new int[ncols][];
 
                 for (var j = 0; j < ncols; j++)
                 {
-                    _grid[i][j] = new int[2];
+                    Grid[i][j] = new int[2];
                 }
             }
 
-            boardInitializer.Initialize(nrows, ncols, nBombs, _grid);
+            boardInitializer.Initialize(nrows, ncols, nBombs, Grid);
 
             Preprocess(nrows, ncols);
         }
+
+        internal int[][][] Grid => _grid;
 
         private void Preprocess(int nrows, int ncols)
         {
             // SS: preprocess grid
             for (var i = 0; i < nrows; i++)
             {
-                var row = _grid[i];
+                var row = Grid[i];
 
                 for (var j = 0; j < ncols; j++)
                 {
@@ -73,7 +79,7 @@ namespace GoogleProblem33
                             continue;
                         }
 
-                        if (_grid[nr][nc][1] == -1)
+                        if (Grid[nr][nc][1] == -1)
                         {
                             nBombs++;
                         }
@@ -85,11 +91,11 @@ namespace GoogleProblem33
             }
         }
 
-        public int[][][] Click(int rowClick, int colClick)
+        public bool Click(int rowClick, int colClick)
         {
             if (rowClick < 0 || rowClick >= _nrows || colClick < 0 || colClick >= _ncols)
             {
-                return _grid;
+                return false;
             }
 
             var queue = new Queue<(int r, int c)>();
@@ -98,7 +104,7 @@ namespace GoogleProblem33
             while (queue.Any())
             {
                 var (r, c) = queue.Dequeue();
-                var cell = _grid[r][c];
+                var cell = Grid[r][c];
 
                 if (cell[0] == 1)
                 {
@@ -108,12 +114,14 @@ namespace GoogleProblem33
 
                 // SS: reveal cell
                 cell[0] = 1;
-
+                nRevealedCells++;
+                
                 if (cell[1] == -1)
                 {
                     // clicked on bomb, game over
-                    PrintGrid(_grid);
+                    PrintGrid(Grid);
                     Console.WriteLine("You lost - Game over");
+                    return false;
                 }
 
                 // SS: if clicked on empty cell, add all its neighbors
@@ -141,7 +149,7 @@ namespace GoogleProblem33
                 }
             }
 
-            return _grid;
+            return nRevealedCells == _nrows * _ncols - _nBombs;
         }
 
         private void PrintGrid(int[][][] grid)
@@ -150,12 +158,12 @@ namespace GoogleProblem33
             {
                 for (int j = 0; j < _ncols; j++)
                 {
-                    var cell = _grid[i][j];
+                    var cell = Grid[i][j];
 
                     if (cell[0] == 0)
                     {
                         // SS: cell has net yet been revealed
-                        Console.Write("   ");
+                        Console.Write("00 ");
                     }
                     else
                     {
@@ -178,19 +186,149 @@ namespace GoogleProblem33
         [TestFixture]
         public class Tests
         {
-            class TestBoardInitializer : IBoardInitializer
+            class TestBoardInitializer1 : IBoardInitializer
             {
-                void IBoardInitializer.Initialize(int nrows, int ncols, int nBombs, int[][][] grid) { }
+                void IBoardInitializer.Initialize(int nrows, int ncols, int nBombs, int[][][] grid)
+                {
+                    // SS: place bomb in cell (0, 4)
+                    grid[0][4][1] = -1;
+                }
             }
 
             [Test]
-            public void Test1()
+            public void TestPreprocessing()
             {
                 // Arrange
+                var boardInitializer = new TestBoardInitializer1();
+                var game = new Minesweeper(2, 8, 1, boardInitializer);
 
                 // Act
+                // Assert
+
+                Assert.AreEqual(1, game.Grid[0][3][1]);
+                Assert.AreEqual(1, game.Grid[0][5][1]);
+                Assert.AreEqual(1, game.Grid[1][3][1]);
+                Assert.AreEqual(1, game.Grid[1][4][1]);
+                Assert.AreEqual(1, game.Grid[1][5][1]);
+
+                // verify that cells are not revealed
+                Assert.AreEqual(0, game.Grid[0][4][0]);
+            }
+
+            [Test]
+            public void Test1MoveOnce()
+            {
+                // Arrange
+                var boardInitializer = new TestBoardInitializer1();
+                var game = new Minesweeper(2, 8, 1, boardInitializer);
+
+                // Act
+                var wonGame = game.Click(0, 0);
 
                 // Assert
+                Assert.False(wonGame);
+                
+                // check that cells are uncovered
+                Assert.AreEqual(1, game.Grid[0][0][0]);
+                Assert.AreEqual(1, game.Grid[0][1][0]);
+                Assert.AreEqual(1, game.Grid[0][2][0]);
+                Assert.AreEqual(1, game.Grid[0][3][0]);
+                Assert.AreEqual(0, game.Grid[0][4][0]);
+                Assert.AreEqual(0, game.Grid[0][5][0]);
+                Assert.AreEqual(0, game.Grid[0][6][0]);
+                Assert.AreEqual(0, game.Grid[0][7][0]);
+                
+                Assert.AreEqual(1, game.Grid[1][0][0]);
+                Assert.AreEqual(1, game.Grid[1][1][0]);
+                Assert.AreEqual(1, game.Grid[1][2][0]);
+                Assert.AreEqual(1, game.Grid[1][3][0]);
+                Assert.AreEqual(0, game.Grid[1][4][0]);
+                Assert.AreEqual(0, game.Grid[1][5][0]);
+                Assert.AreEqual(0, game.Grid[1][6][0]);
+                Assert.AreEqual(0, game.Grid[1][7][0]);
+            }
+
+            [Test]
+            public void Test1MoveTwice()
+            {
+                // Arrange
+                var boardInitializer = new TestBoardInitializer1();
+                var game = new Minesweeper(2, 8, 1, boardInitializer);
+                game.Click(0, 0);
+
+                // Act
+                var wonGame = game.Click(0, 7);
+
+                // Assert
+                Assert.False(wonGame);
+                
+                // check that cells are uncovered
+                Assert.AreEqual(1, game.Grid[0][0][0]);
+                Assert.AreEqual(1, game.Grid[0][1][0]);
+                Assert.AreEqual(1, game.Grid[0][2][0]);
+                Assert.AreEqual(1, game.Grid[0][3][0]);
+                Assert.AreEqual(0, game.Grid[0][4][0]);
+                Assert.AreEqual(1, game.Grid[0][5][0]);
+                Assert.AreEqual(1, game.Grid[0][6][0]);
+                Assert.AreEqual(1, game.Grid[0][7][0]);
+                
+                Assert.AreEqual(1, game.Grid[1][0][0]);
+                Assert.AreEqual(1, game.Grid[1][1][0]);
+                Assert.AreEqual(1, game.Grid[1][2][0]);
+                Assert.AreEqual(1, game.Grid[1][3][0]);
+                Assert.AreEqual(0, game.Grid[1][4][0]);
+                Assert.AreEqual(1, game.Grid[1][5][0]);
+                Assert.AreEqual(1, game.Grid[1][6][0]);
+                Assert.AreEqual(1, game.Grid[1][7][0]);
+            }
+            
+            [Test]
+            public void Test1MoveThree()
+            {
+                // Arrange
+                var boardInitializer = new TestBoardInitializer1();
+                var game = new Minesweeper(2, 8, 1, boardInitializer);
+                game.Click(0, 0);
+                game.Click(0, 7);
+
+                // Act
+                var wonGame = game.Click(1, 4);
+
+                // Assert
+                Assert.True(wonGame);
+                
+                // check that cells are uncovered
+                Assert.AreEqual(1, game.Grid[0][0][0]);
+                Assert.AreEqual(1, game.Grid[0][1][0]);
+                Assert.AreEqual(1, game.Grid[0][2][0]);
+                Assert.AreEqual(1, game.Grid[0][3][0]);
+                Assert.AreEqual(0, game.Grid[0][4][0]);
+                Assert.AreEqual(1, game.Grid[0][5][0]);
+                Assert.AreEqual(1, game.Grid[0][6][0]);
+                Assert.AreEqual(1, game.Grid[0][7][0]);
+                
+                Assert.AreEqual(1, game.Grid[1][0][0]);
+                Assert.AreEqual(1, game.Grid[1][1][0]);
+                Assert.AreEqual(1, game.Grid[1][2][0]);
+                Assert.AreEqual(1, game.Grid[1][3][0]);
+                Assert.AreEqual(1, game.Grid[1][4][0]);
+                Assert.AreEqual(1, game.Grid[1][5][0]);
+                Assert.AreEqual(1, game.Grid[1][6][0]);
+                Assert.AreEqual(1, game.Grid[1][7][0]);
+            }
+
+            [Test]
+            public void TestFail()
+            {
+                // Arrange
+                var boardInitializer = new TestBoardInitializer1();
+                var game = new Minesweeper(2, 8, 1, boardInitializer);
+
+                // Act
+                var wonGame = game.Click(0, 4);
+
+                // Assert
+                Assert.False(wonGame);
             }
         }
     }

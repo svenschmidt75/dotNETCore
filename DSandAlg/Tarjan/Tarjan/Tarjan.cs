daecdbf6423985f9e808bf3190fd8e1166dbda7a
Sven <svenschmidt75@googlemail.com>
2020-10-05 18:05:53 -0600

    cleanup

--- DSandAlg/Tarjan/Tarjan/Tarjan.cs
+++ DSandAlg/Tarjan/Tarjan/Tarjan.cs
@@ -29,18 +29,19 @@ namespace Tarjan
                     continue;
                 }
 
-                visited.Add(i);
-
                 var stack = new Stack<int>();
-                stack.Push(i);
-                var stackSet = new HashSet<int> {i};
-
+                var stackSet = new HashSet<int>();
                 DFS(g, i, visited, stack, stackSet, vertex2Scc);
             }
         }
 
         private void DFS(Graph g, int vertex, HashSet<int> visited, Stack<int> stack, HashSet<int> stackSet, int[] vertex2Scc)
         {
+            visited.Add(vertex);
+            stack.Push(vertex);
+            stackSet.Add(vertex);
+
+            // SS: set initial low-link value
             vertex2Scc[vertex] = vertex;
 
             var neighbors = g.AdjacencyList[vertex];
@@ -53,28 +54,19 @@ namespace Tarjan
                     // SS: update low-link value?
                     if (stackSet.Contains(toVertex))
                     {
-                        // SS: we found a SCC
+                        // SS: we found an SCC
 
                         // propagate smallest low-link value
-                        var lowLink = vertex2Scc[vertex];
-                        var otherLowLink = vertex2Scc[toVertex];
-                        vertex2Scc[vertex] = Math.Min(lowLink, otherLowLink);
+                        vertex2Scc[vertex] = Math.Min(vertex2Scc[vertex], vertex2Scc[toVertex]);
                         break;
                     }
 
                     continue;
                 }
 
-                visited.Add(toVertex);
-
-                stack.Push(toVertex);
-                stackSet.Add(toVertex);
-
                 DFS(g, toVertex, visited, stack, stackSet, vertex2Scc);
 
-                var lowLink2 = vertex2Scc[vertex];
-                var otherLowLink2 = vertex2Scc[toVertex];
-                vertex2Scc[vertex] = Math.Min(lowLink2, otherLowLink2);
+                vertex2Scc[vertex] = Math.Min(vertex2Scc[vertex], vertex2Scc[toVertex]);
             }
 
             // SS: beginning of SCC?
@@ -101,7 +93,7 @@ namespace Tarjan
         [TestFixture]
         public class Tests
         {
-            private Graph Create()
+            private Graph Create1()
             {
                 // SS: from Tarjan's Strongly Connected Component (SCC) Algorithm (UPDATED) | Graph Theory
                 // https://www.youtube.com/watch?v=wUgWX0nc4NY&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=23
@@ -145,7 +137,7 @@ namespace Tarjan
             public void Test1()
             {
                 // Arrange
-                var g = Create();
+                var g = Create1();
 
                 // Act
                 var sccs = new Tarjan().FindSCCs(g);
@@ -161,6 +153,43 @@ namespace Tarjan
 
                 Assert.AreEqual(3, sccs[3]);
                 Assert.AreEqual(3, sccs[7]);
+            }
+
+            [Test]
+            public void TwoCircles()
+            {
+                // Arrange
+                var g = new Graph();
+                g.AddNode(0);
+                g.AddNode(1);
+                g.AddNode(2);
+                g.AddNode(3);
+                g.AddNode(4);
+                g.AddNode(5);
+
+                g.AddDirectedEdge(0, 1);
+
+                g.AddDirectedEdge(1, 2);
+                g.AddDirectedEdge(1, 4);
+
+                g.AddDirectedEdge(2, 3);
+
+                g.AddDirectedEdge(3, 0);
+
+                g.AddDirectedEdge(4, 5);
+
+                g.AddDirectedEdge(5, 0);
+
+                // Act
+                var sccs = new Tarjan().FindSCCs(g);
+
+                // Assert
+                Assert.AreEqual(0, sccs[0]);
+                Assert.AreEqual(0, sccs[1]);
+                Assert.AreEqual(0, sccs[2]);
+                Assert.AreEqual(0, sccs[3]);
+                Assert.AreEqual(0, sccs[4]);
+                Assert.AreEqual(0, sccs[5]);
             }
         }
     }

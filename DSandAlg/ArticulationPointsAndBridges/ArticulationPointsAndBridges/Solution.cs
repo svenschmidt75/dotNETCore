d515b236002a4fdffc3cc637dba124e8e1571642
Sven Schmidt <svenschmidt75@googlemail.com>
2020-10-06 09:21:02 -0600

    start finding articulation points

---
+++ DSandAlg/ArticulationPointsAndBridges/ArticulationPointsAndBridges/Solution.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ArticulationPointsAndBridges
+{
+    public class Solution
+    {
+        private int _time;
+        
+        public int[] GetArticulationPoints(Graph g)
+        {
+            var visited = new HashSet<int>();
+            int[] disc = new int[g.AdjacencyList.Count];
+            int[] low = new int[g.AdjacencyList.Count];
+            int[] ap = new int[g.AdjacencyList.Count];
+
+            int[] parent = new int[g.AdjacencyList.Count];
+            for (int i = 0; i < parent.Length; i++)
+            {
+                parent[i] = -1;
+            }
+
+            // SS: discovery time for the first vertex
+            _time = 1;
+            
+            for (int i = 0; i < g.AdjacencyList.Count; i++)
+            {
+                if (visited.Contains(i))
+                {
+                    continue;
+                }
+                
+                DFS(g, i, visited, disc, low, parent, ap);
+            }
+            
+            return ap;
+        }
+
+        private void DFS(Graph g, int vertex, HashSet<int> visited, int[] disc, int[] low, int[] parent, int[] ap)
+        {
+            visited.Add(vertex);
+
+            // SS: record discovery time
+            disc[vertex] = _time;
+            low[vertex] = _time;
+
+            _time++;
+
+            var neighbors = g.AdjacencyList[vertex];
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                var toVertex = neighbors[i];
+                if (visited.Contains(toVertex))
+                {
+                    if (g.AdjacencyList[vertex].Contains(toVertex) == false)
+                    {
+                        // SS: record the earliest time we can reach from vertex
+                        low[vertex] = Math.Min(low[vertex], disc[toVertex]);
+                    }
+                    continue;
+                }
+
+                parent[toVertex] = vertex;
+                
+                DFS(g, toVertex, visited, disc, low, parent, ap);
+                if (low[toVertex] >= disc[vertex])
+                {
+                    // SS: toVertex does not have a path to an ancestor of vertex,
+                    // so removing vertex would increase the number of connected
+                    // components. Hence, vertex is an articulation point.
+                    ap[vertex] = 1;
+                }
+            }
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            private static Graph CreateGraph1()
+            {
+                // SS: from https://www.hackerearth.com/practice/algorithms/graphs/articulation-points-and-bridges/tutorial/
+                
+                var g = new Graph();
+                g.AddNode(0);
+                g.AddNode(1);
+                g.AddNode(2);
+                g.AddNode(3);
+                g.AddNode(4);
+                g.AddNode(5);
+                
+                g.AddUndirectedEdge(0, 1);
+                g.AddUndirectedEdge(0, 5);
+                
+                g.AddUndirectedEdge(1, 2);
+                g.AddUndirectedEdge(1, 3);
+                
+                g.AddUndirectedEdge(2, 3);
+                g.AddUndirectedEdge(2, 4);
+
+                g.AddUndirectedEdge(3, 4);
+
+                return g;
+            }
+
+            [Test]
+            public void Test1()
+            {
+                // Arrange
+                var g = CreateGraph1();
+                
+                // Act
+                var ap = new Solution().GetArticulationPoints(g);
+                
+                // Assert
+            }
+        }
+    }
+}
--- DSandAlg/Tarjan/Tarjan/Tarjan.cs
+++ DSandAlg/Tarjan/Tarjan/Tarjan.cs
@@ -29,19 +29,18 @@ namespace Tarjan
                     continue;
                 }
 
+                visited.Add(i);
+
                 var stack = new Stack<int>();
-                var stackSet = new HashSet<int>();
+                stack.Push(i);
+                var stackSet = new HashSet<int> {i};
+
                 DFS(g, i, visited, stack, stackSet, vertex2Scc);
             }
         }
 
         private void DFS(Graph g, int vertex, HashSet<int> visited, Stack<int> stack, HashSet<int> stackSet, int[] vertex2Scc)
         {
-            visited.Add(vertex);
-            stack.Push(vertex);
-            stackSet.Add(vertex);
-
-            // SS: set initial low-link value
             vertex2Scc[vertex] = vertex;
 
             var neighbors = g.AdjacencyList[vertex];
@@ -54,19 +53,28 @@ namespace Tarjan
                     // SS: update low-link value?
                     if (stackSet.Contains(toVertex))
                     {
-                        // SS: we found an SCC
+                        // SS: we found a SCC
 
                         // propagate smallest low-link value
-                        vertex2Scc[vertex] = Math.Min(vertex2Scc[vertex], vertex2Scc[toVertex]);
+                        var lowLink = vertex2Scc[vertex];
+                        var otherLowLink = vertex2Scc[toVertex];
+                        vertex2Scc[vertex] = Math.Min(lowLink, otherLowLink);
                         break;
                     }
 
                     continue;
                 }
 
+                visited.Add(toVertex);
+
+                stack.Push(toVertex);
+                stackSet.Add(toVertex);
+
                 DFS(g, toVertex, visited, stack, stackSet, vertex2Scc);
 
-                vertex2Scc[vertex] = Math.Min(vertex2Scc[vertex], vertex2Scc[toVertex]);
+                var lowLink2 = vertex2Scc[vertex];
+                var otherLowLink2 = vertex2Scc[toVertex];
+                vertex2Scc[vertex] = Math.Min(lowLink2, otherLowLink2);
             }
 
             // SS: beginning of SCC?
@@ -93,7 +101,7 @@ namespace Tarjan
         [TestFixture]
         public class Tests
         {
-            private Graph Create1()
+            private Graph Create()
             {
                 // SS: from Tarjan's Strongly Connected Component (SCC) Algorithm (UPDATED) | Graph Theory
                 // https://www.youtube.com/watch?v=wUgWX0nc4NY&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=23
@@ -137,7 +145,7 @@ namespace Tarjan
             public void Test1()
             {
                 // Arrange
-                var g = Create1();
+                var g = Create();
 
                 // Act
                 var sccs = new Tarjan().FindSCCs(g);
@@ -153,43 +161,6 @@ namespace Tarjan
 
                 Assert.AreEqual(3, sccs[3]);
                 Assert.AreEqual(3, sccs[7]);
-            }
-
-            [Test]
-            public void TwoCircles()
-            {
-                // Arrange
-                var g = new Graph();
-                g.AddNode(0);
-                g.AddNode(1);
-                g.AddNode(2);
-                g.AddNode(3);
-                g.AddNode(4);
-                g.AddNode(5);
-
-                g.AddDirectedEdge(0, 1);
-
-                g.AddDirectedEdge(1, 2);
-                g.AddDirectedEdge(1, 4);
-
-                g.AddDirectedEdge(2, 3);
-
-                g.AddDirectedEdge(3, 0);
-
-                g.AddDirectedEdge(4, 5);
-
-                g.AddDirectedEdge(5, 0);
-
-                // Act
-                var sccs = new Tarjan().FindSCCs(g);
-
-                // Assert
-                Assert.AreEqual(0, sccs[0]);
-                Assert.AreEqual(0, sccs[1]);
-                Assert.AreEqual(0, sccs[2]);
-                Assert.AreEqual(0, sccs[3]);
-                Assert.AreEqual(0, sccs[4]);
-                Assert.AreEqual(0, sccs[5]);
             }
         }
     }

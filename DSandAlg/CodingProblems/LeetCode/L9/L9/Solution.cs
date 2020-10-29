e2211e5c26e1c798fa47c8b7885cb19b49d73147
Sven <svenschmidt75@googlemail.com>
2020-10-28 19:16:44 -0600

    LeetCode 9

---
+++ DSandAlg/CodingProblems/LeetCode/L9/L9/Solution.cs
@@ -0,0 +1,82 @@
+﻿#region
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+// 9. Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
+
+namespace L9
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            var s1 = x.ToString();
+
+            var array = s1.ToCharArray();
+            Array.Reverse(array);
+            var s2 = new string(array);
+
+            return s1.Equals(s2);
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void Test1()
+            {
+                // Arrange
+                var x = 121;
+
+                // Act
+                var isPalindrome = new Solution().IsPalindrome(x);
+
+                // Assert
+                Assert.True(isPalindrome);
+            }
+
+            [Test]
+            public void Test2()
+            {
+                // Arrange
+                var x = -121;
+
+                // Act
+                var isPalindrome = new Solution().IsPalindrome(x);
+
+                // Assert
+                Assert.False(isPalindrome);
+            }
+
+            [Test]
+            public void Test3()
+            {
+                // Arrange
+                var x = 10;
+
+                // Act
+                var isPalindrome = new Solution().IsPalindrome(x);
+
+                // Assert
+                Assert.False(isPalindrome);
+            }
+
+            [Test]
+            public void Test4()
+            {
+                // Arrange
+                var x = -101;
+
+                // Act
+                var isPalindrome = new Solution().IsPalindrome(x);
+
+                // Assert
+                Assert.False(isPalindrome);
+            }
+        }
+    }
+}

4eac84c5fb41eb4102b1204b881251085e9c1c5a
Sven <svenschmidt75@googlemail.com>
2020-11-18 17:24:32 -0700

    LeetCode 518 accepted

---
+++ DSandAlg/CodingProblems/LeetCode/L518/L518/Solution.cs
@@ -0,0 +1,143 @@
+#region
+
+using NUnit.Framework;
+
+#endregion
+
+// Problem: 518. Coin Change 2
+// URL: https://leetcode.com/problems/coin-change-2/
+
+namespace LeetCode518
+{
+    public class Solution
+    {
+        public int Change(int amount, int[] coins)
+        {
+            if (amount == 1)
+            {
+                return 0;
+            }
+
+            if (coins.Length == 0)
+            {
+                return 0;
+            }
+
+            return ChangeTopDown(amount, coins);
+        }
+
+        private int ChangeTopDown(int amount, int[] coins)
+        {
+            var dp = new int[amount + 1][];
+            for (var i = 0; i <= amount; i++)
+            {
+                dp[i] = new int[coins.Length];
+            }
+
+            int Change2(int remaining, int[] coins, int idx)
+            {
+                if (remaining == 0)
+                {
+                    return 2;
+                }
+
+                if (idx == coins.Length || remaining < 0)
+                {
+                    return 1;
+                }
+
+                if (dp[remaining][idx] > 0)
+                {
+                    return dp[remaining][idx];
+                }
+
+                var l1 = Change2(remaining - coins[idx], coins, idx) - 1;
+                var l2 = Change2(remaining, coins, idx + 1) - 1;
+
+                var n = l1 + l2 + 1;
+
+                dp[remaining][idx] = n;
+
+                return n;
+            }
+
+            var n = Change2(amount, coins, 0) - 1;
+            return n;
+        }
+
+
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void Test1()
+            {
+                // Arrange
+                int[] coins = {1, 2, 5};
+                var amount = 5;
+
+                // Act
+                var n = new Solution().Change(amount, coins);
+
+                // Assert
+                Assert.AreEqual(4, n);
+            }
+
+            [Test]
+            public void Test2()
+            {
+                // Arrange
+                int[] coins = {2};
+                var amount = 3;
+
+                // Act
+                var n = new Solution().Change(amount, coins);
+
+                // Assert
+                Assert.AreEqual(0, n);
+            }
+
+            [Test]
+            public void Test3()
+            {
+                // Arrange
+                int[] coins = {10};
+                var amount = 10;
+
+                // Act
+                var n = new Solution().Change(amount, coins);
+
+                // Assert
+                Assert.AreEqual(1, n);
+            }
+
+            [Test]
+            public void Test4()
+            {
+                // Arrange
+                int[] coins = {52, 480, 116, 409, 170, 240, 496};
+                var amount = 8230;
+
+                // Act
+                var n = new Solution().Change(amount, coins);
+
+                // Assert
+                Assert.AreEqual(39197, n);
+            }
+
+            [Test]
+            public void Test5()
+            {
+                // Arrange
+                int[] coins = { };
+                var amount = 0;
+
+                // Act
+                var n = new Solution().Change(amount, coins);
+
+                // Assert
+                Assert.AreEqual(1, n);
+            }
+        }
+    }
+}

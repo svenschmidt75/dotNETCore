7f32b6a56a6d91ebc6e7233bf81fc606a49e9994
Sven <svenschmidt75@googlemail.com>
2020-12-15 07:39:22 -0700

    LeetCode 96

---
+++ DSandAlg/CodingProblems/LeetCode/L96/L96/Solution.cs
@@ -0,0 +1,76 @@
+#region
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+// Problem: 96. Unique Binary Search Trees
+// URL: https://leetcode.com/problems/unique-binary-search-trees/
+
+namespace Leetcode
+{
+    public class Solution
+    {
+        public int NumTrees(int n)
+        {
+            // SS: runtime performance: O(1 + 1 + 3 + 4 + ... + n) = O(N^2)
+            // The idea is that for n = 4, we iterate over 1, 2, 3, 4 and for
+            // each iteration, we have a sub problem on the left and right of
+            // a size we have already solved, so we just lookup.
+            // n = 4, i.e. P(4):
+            //      1             2            3            4      
+            //    /   \         /   \        /   \        /   \
+            //  P(0)  P(3)   P(1)   P(2)   P(2)  P(1)   P(3)  P(0)
+            // etc.
+
+            if (n <= 2)
+            {
+                return n;
+            }
+
+            var dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (var i = 3; i <= n; i++)
+            {
+                var total = 0;
+
+                for (var j = 1; j <= i; j++)
+                {
+                    var left = Math.Max(1, j - 1);
+                    var right = Math.Max(1, i - j);
+                    var v = dp[left] * dp[right];
+                    total += v;
+                }
+
+                dp[i] = total;
+            }
+
+            return dp[n];
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            [TestCase(1, 1)]
+            [TestCase(2, 2)]
+            [TestCase(3, 5)]
+            [TestCase(4, 14)]
+            [TestCase(10, 16796)]
+            [TestCase(19, 1767263190)]
+            public void Test1(int n, int expectedCount)
+            {
+                // Arrange
+
+                // Act
+                var count = new Solution().NumTrees(n);
+
+                // Assert
+                Assert.AreEqual(expectedCount, count);
+            }
+        }
+    }
+}

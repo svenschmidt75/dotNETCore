b8ee572c7793848020dc9d7956cf30efd250b5e0
Sven <svenschmidt75@googlemail.com>
2020-12-19 12:18:05 -0700

    LeetCode 53: add D&Q solution

--- DSandAlg/CodingProblems/LeetCode/L53/L53/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L53/L53/Solution.cs
@@ -14,6 +14,11 @@ namespace LeetCode
     {
         public int MaxSubArray(int[] nums)
         {
+            return MaxSubArrayDQ(nums);
+        }
+
+        public int MaxSubArray1(int[] nums)
+        {
             // SS: Kadane's algorithm, O(1)
             if (nums.Length == 1)
             {
@@ -27,20 +32,70 @@ namespace LeetCode
             while (i < nums.Length)
             {
                 var v = nums[i];
-                if (v > sum + v)
-                {
-                    sum = v;
-                }
-                else
-                {
-                    sum += v;
-                }
+                // if (v > sum + v)
+                // {
+                //     sum = v;
+                // }
+                // else
+                // {
+                //     sum += v;
+                // }
+                sum = Math.Max(sum, 0) + v;
 
                 bestSum = Math.Max(bestSum, sum);
 
                 i++;
             }
 
+            return bestSum;
+        }
+
+        public int MaxSubArray2(int[] nums)
+        {
+            // SS: Kadane's algorithm, O(1)
+            // in-place, dynamic programming
+            // recurrence relation: dp[i] = Math.Max(dp[i - 1], 0) + dp[i]
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            var maxSum = nums[0];
+            for (var j = 1; j < nums.Length; j++)
+            {
+                nums[j] = Math.Max(nums[j - 1], 0) + nums[j];
+                maxSum = Math.Max(maxSum, nums[j]);
+            }
+
+            return maxSum;
+        }
+
+        public int MaxSubArrayDQ(int[] nums)
+        {
+            // SS: Divide & Conquer
+            // dp[i] = Math.Max(dp[i - 1], 0) + dp[i]
+
+            var bestSum = nums[0];
+
+            if (nums.Length == 1)
+            {
+                return bestSum;
+            }
+
+            int Solve(int idx)
+            {
+                if (idx == 0)
+                {
+                    return nums[0];
+                }
+
+                var b = Solve(idx - 1);
+                var best = Math.Max(b, 0) + nums[idx];
+                bestSum = Math.Max(bestSum, best);
+                return best;
+            }
+
+            Solve(nums.Length - 1);
             return bestSum;
         }
 
@@ -84,6 +139,19 @@ namespace LeetCode
 
                 // Assert
                 Assert.AreEqual(122, maxSum);
+            }
+
+            [Test]
+            public void Test4()
+            {
+                // Arrange
+                int[] nums = {-1, -2};
+
+                // Act
+                var maxSum = new Solution().MaxSubArray(nums);
+
+                // Assert
+                Assert.AreEqual(-1, maxSum);
             }
         }
     }

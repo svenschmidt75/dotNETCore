51ff8d2786afa6d6b9d653e6ee2a03f29a965998
Sven <svenschmidt75@googlemail.com>
2020-12-24 07:19:02 -0700

    LeetCode 87: faster solution

--- DSandAlg/CodingProblems/LeetCode/L87/L87/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L87/L87/Solution.cs
@@ -1,5 +1,6 @@
 #region
 
+using System.Collections.Generic;
 using NUnit.Framework;
 
 #endregion
@@ -13,24 +14,66 @@ namespace LeetCode
     {
         public bool IsScramble(string s1, string s2)
         {
-            bool Solve(string s, string suffix)
+            return IsScrambleFast(s1, s2);
+        }
+
+        public bool IsScrambleFast(string s1, string s2)
+        {
+            // https://leetcode.com/problems/scramble-string/discuss/29392/Share-my-4ms-c%2B%2B-recursive-solution
+            // scramble both strings until they are equal
+
+            // SS: this check is important (i.e. terminating condition)
+            if (s1 == s2) return true;
+
+            // SS: check each character occurs with the same
+            // frequency between the two strings.
+            // This is a filtering step...
+            var len = s1.Length;
+            var count = new int[26];
+            for (var i = 0; i < len; i++)
+            {
+                count[s1[i] - 'a']++;
+                count[s2[i] - 'a']--;
+            }
+
+            // if un-match, return false;
+            for (var i = 0; i < 26; i++)
+            {
+                if (count[i] != 0) return false;
+            }
+
+            for (var i = 1; i <= len - 1; i++)
+            {
+                if (IsScramble(s1.Substring(0, i), s2.Substring(0, i)) && IsScramble(s1.Substring(i), s2.Substring(i))) return true;
+
+                if (IsScramble(s1.Substring(0, i), s2.Substring(len - i)) && IsScramble(s1.Substring(i), s2.Substring(0, len - i))) return true;
+            }
+
+            return false;
+        }
+
+        public bool IsScrambleDivideConquer(string s1, string s2)
+        {
+            // SS: runtime complexity: O(2^n)
+
+            bool Solve(string s, int idx)
             {
                 if (s.Length == 1)
                 {
-                    return s + suffix == s2;
+                    return s[0] == s2[idx];
                 }
-                
+
                 for (var i = 1; i < s.Length; i++)
                 {
                     var s11 = s[..i];
                     var s12 = s[i..];
 
-                    if (Solve(s11, s12 + suffix))
+                    if (Solve(s11, idx) && Solve(s12, idx + s11.Length))
                     {
                         return true;
                     }
 
-                    if (Solve(s12, s11 + suffix))
+                    if (Solve(s12, idx) && Solve(s11, idx + s12.Length))
                     {
                         return true;
                     }
@@ -39,7 +82,54 @@ namespace LeetCode
                 return false;
             }
 
-            return Solve(s1, "");
+            return Solve(s1, 0);
+        }
+
+        public bool IsScrambleTopDown(string s1, string s2)
+        {
+            // SS: runtime complexity: 
+
+            var dict = new Dictionary<string, bool>[s1.Length];
+            for (var i = 0; i < s1.Length; i++)
+            {
+                dict[i] = new Dictionary<string, bool>();
+            }
+
+            bool Solve(string s, int idx)
+            {
+                if (s.Length == 1)
+                {
+                    return s[0] == s2[idx];
+                }
+
+                if (dict[idx].TryGetValue(s, out var v))
+                {
+                    return v;
+                }
+
+                for (var i = 1; i < s.Length; i++)
+                {
+                    var s11 = s[..i];
+                    var s12 = s[i..];
+
+                    if (Solve(s11, idx) && Solve(s12, idx + s11.Length))
+                    {
+                        dict[idx][s] = true;
+                        return true;
+                    }
+
+                    if (Solve(s12, idx) && Solve(s11, idx + s12.Length))
+                    {
+                        dict[idx][s] = true;
+                        return true;
+                    }
+                }
+
+                dict[idx][s] = false;
+                return false;
+            }
+
+            return Solve(s1, 0);
         }
 
         [TestFixture]
@@ -79,6 +169,62 @@ namespace LeetCode
                 // Arrange
                 var s1 = "a";
                 var s2 = "a";
+
+                // Act
+                var result = new Solution().IsScramble(s1, s2);
+
+                // Assert
+                Assert.True(result);
+            }
+
+            [Test]
+            public void Test4()
+            {
+                // Arrange
+                var s1 = "greatgreatgreatgreat";
+                var s2 = "taregtaregtaregtareg";
+
+                // Act
+                var result = new Solution().IsScramble(s1, s2);
+
+                // Assert
+                Assert.True(result);
+            }
+
+            [Test]
+            public void Test5()
+            {
+                // Arrange
+                var s1 = "abc";
+                var s2 = "acb";
+
+                // Act
+                var result = new Solution().IsScramble(s1, s2);
+
+                // Assert
+                Assert.True(result);
+            }
+
+            [Test]
+            public void Test6()
+            {
+                // Arrange
+                var s1 = "abcd";
+                var s2 = "badc";
+
+                // Act
+                var result = new Solution().IsScramble(s1, s2);
+
+                // Assert
+                Assert.True(result);
+            }
+
+            [Test]
+            public void Test7()
+            {
+                // Arrange
+                var s1 = "great";
+                var s2 = "rgtae";
 
                 // Act
                 var result = new Solution().IsScramble(s1, s2);

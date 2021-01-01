a903585e2cd3d2635f3bdf63dcfd7725e6f7e280
Sven <svenschmidt75@googlemail.com>
2021-01-01 16:13:22 -0700

    LeetCode 127: accepted, using BFS

--- DSandAlg/CodingProblems/LeetCode/L127/L127/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L127/L127/Solution.cs
@@ -16,15 +16,107 @@ namespace LeetCode
     {
         public int LadderLength(string beginWord, string endWord, IList<string> wordList)
         {
-            return LadderLengthBacktracking(beginWord, endWord, wordList);
+//            return LadderLengthBacktracking(beginWord, endWord, wordList);
+            return LadderLengthShortestPath(beginWord, endWord, wordList);
+        }
+
+        private int LadderLengthShortestPath(string beginWord, string endWord, IList<string> wordList)
+        {
+            // SS: runtime complexity: O(N^2 * beginWord), N = wordList.Length
+            // space complexity: O(N^2) (per vertex N, store up to N vertices)
+
+            // SS: is the end word contained?
+            if (wordList.IndexOf(endWord) == -1)
+            {
+                // SS: no solution
+                return 0;
+            }
+
+            var endVertex = -1;
+
+            // SS: generate graph with only valid transitions (i.e. two words are connected if
+            // they differ by at most 1 character)
+            var g = new Graph();
+            for (var i = 0; i <= wordList.Count; i++)
+            {
+                g.AddVertex(i);
+            }
+
+            // SS: O(N * beginWord)
+            for (var i = 0; i < wordList.Count; i++)
+            {
+                var w = wordList[i];
+                if (w == endWord)
+                {
+                    endVertex = i + 1;
+                }
+
+                // SS: O(beginWord)
+                if (Difference(beginWord, w))
+                {
+                    g.AddUndirectedEdge(0, i + 1);
+                }
+            }
+
+            // SS: O(N^2 * beginWord)
+            for (var i = 0; i < wordList.Count; i++)
+            {
+                var w1 = wordList[i];
+
+                for (var j = i + 1; j < wordList.Count; j++)
+                {
+                    var w2 = wordList[j];
+
+                    if (Difference(w1, w2))
+                    {
+                        g.AddUndirectedEdge(i + 1, j + 1);
+                    }
+                }
+            }
+
+            // SS: do BFS starting from vertex 0
+            // O(N)
+            var queue = new Queue<(int vertex, int pathLength)>();
+            queue.Enqueue((0, 1));
+
+            var visited = new HashSet<int> {0};
+
+            while (queue.Any())
+            {
+                (var vertex, var pathLength) = queue.Dequeue();
+
+                if (vertex == endVertex)
+                {
+                    return pathLength;
+                }
+
+                var adjList = g.AdjacencyList[vertex];
+                for (var i = 0; i < adjList.Count; i++)
+                {
+                    var target = adjList[i];
+
+                    if (visited.Contains(target))
+                    {
+                        continue;
+                    }
+
+                    visited.Add(target);
+
+                    queue.Enqueue((target, pathLength + 1));
+                }
+            }
+
+            return 0;
         }
 
         private int LadderLengthBacktracking(string beginWord, string endWord, IList<string> wordList)
         {
             // SS: check for valid input
 
-            int Solve(string currentWord, IList<string> wordsLeft, IList<string> wordsSoFar)
+            int Solve(IList<string> wordsLeft, IList<string> wordsSoFar)
             {
+                var currentWord = wordsSoFar[^1];
+
                 // SS: termination condition
                 if (currentWord == endWord)
                 {
@@ -48,7 +140,7 @@ namespace LeetCode
                     {
                         wordsSoFar.Add(w);
                         wordsLeft.Remove(w);
-                        var c = Solve(w, wordsLeft, wordsSoFar);
+                        var c = Solve(wordsLeft, wordsSoFar);
                         minCount = Math.Min(minCount, c);
 
                         // SS: backtrack
@@ -62,11 +154,11 @@ namespace LeetCode
 
             // SS: could check if end word is on wordList...
 
-            var n = Solve(beginWord, wordList, new List<string> {beginWord});
+            var n = Solve(wordList, new List<string> {beginWord});
             return n == int.MaxValue ? 0 : n;
         }
 
-        private bool Difference(string w1, string w2)
+        private static bool Difference(string w1, string w2)
         {
             var isDifferent = false;
 
@@ -84,6 +176,28 @@ namespace LeetCode
             }
 
             return true;
+        }
+
+        public class Graph
+        {
+            public IDictionary<int, List<int>> AdjacencyList { get; } = new Dictionary<int, List<int>>();
+
+            public void AddVertex(int vertexId)
+            {
+                AdjacencyList[vertexId] = new List<int>();
+            }
+
+            public void AddDirectedEdge(int fromVertexId, int toVertexId)
+            {
+                AdjacencyList[fromVertexId].Add(toVertexId);
+            }
+
+
+            public void AddUndirectedEdge(int fromVertexId, int toVertexId)
+            {
+                AddDirectedEdge(fromVertexId, toVertexId);
+                AddDirectedEdge(toVertexId, fromVertexId);
+            }
         }
 
         [TestFixture]
@@ -107,6 +221,32 @@ namespace LeetCode
             {
                 // Arrange
                 var wordList = new List<string> {"hot", "dot", "dog", "lot", "log"};
+
+                // Act
+                var n = new Solution().LadderLength("hit", "cog", wordList);
+
+                // Assert
+                Assert.AreEqual(0, n);
+            }
+
+            [Test]
+            public void Test3()
+            {
+                // Arrange
+                var wordList = new List<string> {"hit"};
+
+                // Act
+                var n = new Solution().LadderLength("hot", "hit", wordList);
+
+                // Assert
+                Assert.AreEqual(2, n);
+            }
+
+            [Test]
+            public void Test4()
+            {
+                // Arrange
+                var wordList = new List<string> {"hot"};
 
                 // Act
                 var n = new Solution().LadderLength("hit", "cog", wordList);

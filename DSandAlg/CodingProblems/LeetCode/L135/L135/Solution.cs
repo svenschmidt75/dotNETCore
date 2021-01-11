9c1c7b96d6f29fb99460769fcdbfc98ca7a4e6de
Sven <svenschmidt75@googlemail.com>
2021-01-11 07:45:23 -0700

    LeetCode 135: accepted

--- DSandAlg/CodingProblems/LeetCode/L135/L135/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L135/L135/Solution.cs
@@ -14,31 +14,22 @@ namespace LeetCode
     {
         public int Candy(int[] ratings)
         {
+            // SS: 2-pass algorithm, i.e. O(N) runtime complexity
+            // O(N) space complexity
+
             if (ratings.Length == 0)
             {
                 return 0;
             }
 
             var candy = new int[ratings.Length];
-            var correction = new int[ratings.Length];
 
             candy[^1] = 1;
 
+            // SS: 1st pass to correct from right to left
             for (var i = ratings.Length - 2; i >= 0; i--)
             {
-                if (ratings[i] < ratings[i + 1])
-                {
-                    var c = candy[i + 1] - 1;
-                    if (c == 0)
-                    {
-                        // SS: add correction
-                        correction[i + 1] = 1;
-                        c = 1;
-                    }
-
-                    candy[i] = c;
-                }
-                else if (ratings[i] == ratings[i + 1])
+                if (ratings[i] <= ratings[i + 1])
                 {
                     candy[i] = Math.Min(1, candy[i + 1]);
                 }
@@ -48,35 +39,18 @@ namespace LeetCode
                 }
             }
 
-            // SS: calculate candies, with correction
-            int j = 0;
-            while (j < correction.Length)
+            // SS: 2nd pass to correct from left to right
+            for (var i = 1; i < candy.Length; i++)
             {
-                if (correction[j] == 1)
+                if (ratings[i - 1] < ratings[i] && candy[i - 1] >= candy[i])
                 {
-                    // SS: correction[0] == 0 always
-                    int delta = 0;
-                    while (j < correction.Length)
-                    {
-                        if (correction[j] == 1 || candy[j - 1] < candy[j])
-                        {
-                            delta += correction[j];
-//                            candy[j] += delta;
-                            correction[j] = 1;
-                            j++;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
+                    candy[i] = candy[i - 1] + 1;
                 }
-
-                j++;
             }
 
-            int nCandy = 0;
-            for (int i = 0; i < candy.Length; i++)
+            // SS: sum candies
+            var nCandy = 0;
+            for (var i = 0; i < candy.Length; i++)
             {
                 nCandy += candy[i];
             }
@@ -91,10 +65,10 @@ namespace LeetCode
             public void Test1()
             {
                 // Arrange
-                int[] ratings =  {1,0,2};
+                int[] ratings = {1, 0, 2};
 
                 // Act
-                int nCandy = new Solution().Candy(ratings);
+                var nCandy = new Solution().Candy(ratings);
 
                 // Assert
                 Assert.AreEqual(5, nCandy);
@@ -104,76 +78,77 @@ namespace LeetCode
             public void Test2()
             {
                 // Arrange
-                int[] ratings =  {1,2,2};
+                int[] ratings = {1, 2, 2};
 
                 // Act
-                int nCandy = new Solution().Candy(ratings);
+                var nCandy = new Solution().Candy(ratings);
 
                 // Assert
                 Assert.AreEqual(4, nCandy);
             }
 
-            [TestCase(new[]{5}, 1)]
-            [TestCase(new[]{3, 5}, 3)]
-            [TestCase(new[]{9, 3, 5}, 5)]
-            [TestCase(new[]{2, 9, 3, 5}, 6)]
-            [TestCase(new[]{1, 2, 9, 3, 5}, 9)]
-            [TestCase(new[]{6, 1, 2, 9, 3, 5}, 11)]
-            [TestCase(new[]{4, 6, 1, 2, 9, 3, 5}, 12)]
-            [TestCase(new[]{0, 4, 6, 1, 2, 9, 3, 5}, 15)]
-            [TestCase(new[]{1, 0, 4, 6, 1, 2, 9, 3, 5}, 17)]
+            [TestCase(new[] {5}, 1)]
+            [TestCase(new[] {3, 5}, 3)]
+            [TestCase(new[] {9, 3, 5}, 5)]
+            [TestCase(new[] {2, 9, 3, 5}, 6)]
+            [TestCase(new[] {1, 2, 9, 3, 5}, 9)]
+            [TestCase(new[] {6, 1, 2, 9, 3, 5}, 11)]
+            [TestCase(new[] {4, 6, 1, 2, 9, 3, 5}, 12)]
+            [TestCase(new[] {0, 4, 6, 1, 2, 9, 3, 5}, 15)]
+            [TestCase(new[] {1, 0, 4, 6, 1, 2, 9, 3, 5}, 17)]
             public void Test3(int[] ratings, int expectedCandy)
             {
                 // Arrange
 
                 // Act
-                int nCandy = new Solution().Candy(ratings);
+                var nCandy = new Solution().Candy(ratings);
 
                 // Assert
                 Assert.AreEqual(expectedCandy, nCandy);
             }
 
-            [TestCase(new[]{5}, 1)]
-            [TestCase(new[]{6, 5}, 3)]
-            [TestCase(new[]{7, 6, 5}, 6)]
-            [TestCase(new[]{8, 7, 6, 5}, 10)]
+            [TestCase(new[] {5}, 1)]
+            [TestCase(new[] {6, 5}, 3)]
+            [TestCase(new[] {7, 6, 5}, 6)]
+            [TestCase(new[] {8, 7, 6, 5}, 10)]
             public void Test4(int[] ratings, int expectedCandy)
             {
                 // Arrange
 
                 // Act
-                int nCandy = new Solution().Candy(ratings);
+                var nCandy = new Solution().Candy(ratings);
 
                 // Assert
                 Assert.AreEqual(expectedCandy, nCandy);
             }
 
-            [TestCase(new[]{1, 2, 3}, 6)]
+            [TestCase(new[] {1, 2, 3}, 6)]
             public void Test5(int[] ratings, int expectedCandy)
             {
                 // Arrange
 
                 // Act
-                int nCandy = new Solution().Candy(ratings);
+                var nCandy = new Solution().Candy(ratings);
 
                 // Assert
                 Assert.AreEqual(expectedCandy, nCandy);
             }
 
-            [TestCase(new[]{2, 2, 1}, 4)]
-            [TestCase(new[]{1, 2, 2, 1}, 6)]
-            [TestCase(new[]{1, 3, 2, 2, 1}, 7)]
+            [TestCase(new[] {2, 2, 1}, 4)]
+            [TestCase(new[] {1, 2, 2, 1}, 6)]
+            [TestCase(new[] {1, 3, 2, 2, 1}, 7)]
+            [TestCase(new[] {1, 2, 8, 9, 3, 5}, 13)]
+            [TestCase(new[] {5, 3, 9, 8, 2, 1}, 13)]
             public void Test6(int[] ratings, int expectedCandy)
             {
                 // Arrange
 
                 // Act
-                int nCandy = new Solution().Candy(ratings);
+                var nCandy = new Solution().Candy(ratings);
 
                 // Assert
                 Assert.AreEqual(expectedCandy, nCandy);
             }
-            
         }
     }
 }

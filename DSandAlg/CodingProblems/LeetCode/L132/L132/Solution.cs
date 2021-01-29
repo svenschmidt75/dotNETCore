fc9f818a4d40162662415f06a6d243d7d43d7591
Sven <svenschmidt75@googlemail.com>
2021-01-29 08:10:11 -0700

    LeetCode 132: DP bottom-up, O(s^3)

--- DSandAlg/CodingProblems/LeetCode/L132/L132/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L132/L132/Solution.cs
@@ -14,7 +14,14 @@ namespace LeetCode
     {
         public int MinCut(string s)
         {
-            return MinCutSlow(s);
+            // SS: There are two basic approaches:
+            // 1: You somehow try to find where the cuts occur with some algorithm
+            // 2: At each step, you assume both situations and try to find ways to
+            //    manage the exponential complexity. Sometimes, DP can be used to
+            //    do so, as was done here.
+
+            //            return MinCutSlow(s);
+            return MinCutBottomUp(s);
         }
 
         private int MinCutSlow(string s)
@@ -22,8 +29,8 @@ namespace LeetCode
             // SS: Divide & Conquer
             // runtime complexity: O(2^N * N), at each position, we have two paths,
             // and we need to check whether a substring is a palindrome
-            
-            int Solve(int idx, int startIdx)
+
+            int Solve(int startIdx, int idx)
             {
                 // SS: base case
                 if (idx == s.Length)
@@ -33,7 +40,7 @@ namespace LeetCode
                         // SS: empty string, so "valid" palindrome, no split
                         return 0;
                     }
-                    
+
                     // SS: check whether valid palindrome in [startIdx, s.Length - 1]
                     if (IsPalindrome(s, startIdx, s.Length - 1))
                     {
@@ -54,13 +61,65 @@ namespace LeetCode
                 }
 
                 // SS: assume no break
-                var c2 = Solve(idx + 1, startIdx);
+                var c2 = Solve(startIdx, idx + 1);
 
                 return Math.Min(c1, c2);
             }
 
             var minCuts = Solve(0, 0);
             return minCuts;
+        }
+
+        private int MinCutBottomUp(string s)
+        {
+            // SS: bottom-up DP, runtime complexity: O(s^3)
+            // space complexity: O(1)
+
+            var dp1 = new int[s.Length + 1];
+            var dp2 = new int[s.Length + 1];
+
+            // SS: set boundary conditions
+            for (var startIdx = s.Length - 1; startIdx >= 0; startIdx--)
+            {
+                if (IsPalindrome(s, startIdx, s.Length - 1))
+                {
+                    // SS: if valid palindrome, no split
+                    dp2[startIdx] = 0;
+                }
+                else
+                {
+                    // SS: not a palindrome, so invalid case, do not count
+                    dp2[startIdx] = int.MaxValue;
+                }
+            }
+
+            // SS: fill in grid
+            for (var idx = s.Length - 1; idx >= 0; idx--)
+            {
+                for (var startIdx = idx; startIdx >= 0; startIdx--)
+                {
+                    // SS: assume cut at idx, if [startIdx, idx] is a palindrome
+                    // since we can only cut when we have a palindrome
+                    var c1 = int.MaxValue;
+                    if (IsPalindrome(s, startIdx, idx))
+                    {
+                        c1 = 1 + dp2[idx + 1];
+                    }
+
+                    // SS: assume no break
+                    var c2 = dp2[startIdx];
+
+                    var c = Math.Min(c1, c2);
+                    dp1[startIdx] = c;
+                }
+
+                // SS: swap
+                var tmp = dp1;
+                dp1 = dp2;
+                dp2 = tmp;
+            }
+
+            return dp2[0];
         }
 
         internal static bool IsPalindrome(string s, int min, int max)

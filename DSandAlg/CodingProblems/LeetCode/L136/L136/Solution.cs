265caa579e2db2e8b2483f3b9c22b0ce81ee103a
Sven <svenschmidt75@googlemail.com>
2021-02-12 06:15:22 -0700

    move files

--- DSandAlg/CodingProblems/LeetCode/L136/L136/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L136/L136/Solution.cs
@@ -1,10 +1,4 @@
-#region
-
-using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
-
-#endregion
 
 // Problem: 136. Single Number
 // URL: https://leetcode.com/problems/single-number/
@@ -15,40 +9,15 @@ namespace LeetCode
     {
         public int SingleNumber(int[] nums)
         {
-            var freqMap = new Dictionary<int, int>();
-
-            for (var i = 0; i < nums.Length; i++)
+            int n = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                var v = nums[i];
-                if (freqMap.TryGetValue(v, out var freq))
-                {
-                    freqMap[v]--;
-                    if (freq == 1)
-                    {
-                        freqMap.Remove(v);
-                    }
-                }
-                else
-                {
-                    freqMap[v] = 1;
-                }
+                n ^= nums[i];
             }
 
-            var values = freqMap.Keys.ToArray();
-            return values[0];
+            return n;
         }
-
-        public int SingleNumberXorTrick(int[] nums)
-        {
-            var count = 0;
-            for (var i = 0; i < nums.Length; i++)
-            {
-                count ^= nums[i];
-            }
-
-            return count;
-        }
-
+        
         [TestFixture]
         public class Tests
         {
@@ -56,26 +25,10 @@ namespace LeetCode
             public void Test1()
             {
                 // Arrange
-                int[] nums = {2, 2, 1};
 
                 // Act
-                var n = new Solution().SingleNumber(nums);
 
                 // Assert
-                Assert.AreEqual(1, n);
-            }
-
-            [Test]
-            public void Test2()
-            {
-                // Arrange
-                int[] nums = {4, 1, 2, 1, 2};
-
-                // Act
-                var n = new Solution().SingleNumber(nums);
-
-                // Assert
-                Assert.AreEqual(4, n);
             }
         }
     }

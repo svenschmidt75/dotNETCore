22d6c1fc8780794c616e894a013b431668244746
Sven <svenschmidt75@googlemail.com>
2021-04-26 07:19:49 -0600

    LeetCode 282

--- DSandAlg/CodingProblems/LeetCode/L282/L282/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L282/L282/Solution.cs
@@ -1,6 +1,9 @@
-using System;
+#region
+
 using System.Collections.Generic;
 using NUnit.Framework;
+
+#endregion
 
 // Problem: 282. Expression Add Operators
 // URL: https://leetcode.com/problems/expression-add-operators/
@@ -18,43 +21,64 @@ namespace LeetCode
         {
             var result = new List<string>();
 
-            void Solve(int sIdx, Func<int, int> eval)
+            void Solve(int sIdx, long value, string str, long prevValue)
             {
                 if (sIdx == num.Length)
                 {
-                    if (eval == target)
+                    if (value == target)
                     {
                         // SS: skip the 1st operator
-                        result.Add(v.Substring(1));
+                        result.Add(str.Substring(1));
                     }
+
                     return;
                 }
 
-                int b1 = 0;
-                int b2 = int.Parse(num[sIdx..]); 
-                for (int i = sIdx; i < num.Length; i++)
+                long b = 0;
+                for (var i = sIdx; i < num.Length; i++)
                 {
-                    char d = num[i];
-                    b1 = b1 * 10 + (d - '0');
-                    b2 -= (d - '0') * (int)Math.Pow(10, num.Length - 1);
+                    var d = num[i];
+                    b = b * 10 + (d - '0');
+
+                    // SS: for *, revert previous operation as * has higher precedence
+                    if (sIdx > 0)
+                    {
+                        Solve(i + 1, value - prevValue + prevValue * b, $"{str}*{b}", prevValue * b);
+                    }
+
+                    // SS: +
+                    Solve(i + 1, value + b, $"{str}+{b}", b);
+
+                    if (sIdx > 0)
+                    {
+                        // SS: -
+                        Solve(i + 1, value - b, $"{str}-{b}", -b);
+                    }
                     
-                    Solve(i + 1, arg => eval(b1 * arg));
+                    // SS: do not allow 08
+                    if (b == 0)
+                    {
+                        break;
+                    }
                 }
             }
-            
-            Solve(0, 0, "");
 
+            Solve(0, 0, "", 0);
             return result;
         }
 
         [TestFixture]
         public class Tests
         {
-            [TestCase("123", 6, new[]{"1*2*3", "1+2+3"})]
-            [TestCase("232", 8, new[]{"2*3+2", "2+3*2"})]
-            [TestCase("00", 0, new[]{"0*0", "0+0", "0-0"})]
+            [TestCase("123", 6, new[] {"1*2*3", "1+2+3"})]
+            [TestCase("232", 8, new[] {"2*3+2", "2+3*2"})]
+            [TestCase("00", 0, new[] {"0*0", "0+0", "0-0"})]
             [TestCase("3456237490", 9191, new string[0])]
-            [TestCase("5", 5, new []{"5"})]
+            [TestCase("5", 5, new[] {"5"})]
+            [TestCase("12412", -45, new[] {"1+2-4*12"})]
+            [TestCase("1241279", -342, new[] {"1+2-4*12*7-9"})]
+            [TestCase("00089", 72, new[] {"0*0*0+8*9","0*0+0+8*9","0*0-0+8*9","0+0*0+8*9","0+0+0+8*9","0+0-0+8*9","0-0*0+8*9","0-0+0+8*9","0-0-0+8*9"})]
+            [TestCase("2147483647", int.MaxValue, new[] {"2147483647"})]
             public void Test(string num, int target, IList<string> expected)
             {
                 // Arrange

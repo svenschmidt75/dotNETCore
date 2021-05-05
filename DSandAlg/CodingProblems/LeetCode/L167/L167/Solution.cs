dfbc6f3273877576111a3a678709518e9b17787c
Sven <svenschmidt75@googlemail.com>
2021-05-05 07:08:03 -0600

    LeetCode 167: add sliding window solution

--- DSandAlg/CodingProblems/LeetCode/L167/L167/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L167/L167/Solution.cs
@@ -1,5 +1,9 @@
+#region
+
 using System.Collections.Generic;
 using NUnit.Framework;
+
+#endregion
 
 // Problem: 167. Two Sum II - Input array is sorted
 // URL: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
@@ -10,7 +14,38 @@ namespace LeetCode
     {
         public int[] TwoSum(int[] numbers, int target)
         {
-            return TwoSum1(numbers, target);
+//            return TwoSum1(numbers, target);
+            return TwoSum2(numbers, target);
+        }
+
+        private int[] TwoSum2(int[] numbers, int target)
+        {
+            // SS: sliding window approach
+            // runtime complexity: O(n)
+            // space complexity: O(1)
+            
+            var i = 0;
+            var j = numbers.Length - 1;
+
+            while (i < j)
+            {
+                var n = numbers[i] + numbers[j];
+
+                if (n > target)
+                {
+                    j--;
+                }
+                else if (n < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    return new[] {i + 1, j + 1};
+                }
+            }
+
+            return new int[0];
         }
 
         private int[] TwoSum1(int[] numbers, int target)
@@ -18,15 +53,15 @@ namespace LeetCode
             // SS: here we are not utilizing the fact that numbers is sorted
             // runtime complexity: O(n)
             // space complexity: O(n)
-            
+
             var map = new Dictionary<int, int>();
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (var i = 0; i < numbers.Length; i++)
             {
-                int v1 = numbers[i];
-                int d = target - v1;
-                
-                if (map.TryGetValue(v1, out int idx))
+                var v1 = numbers[i];
+                var d = target - v1;
+
+                if (map.TryGetValue(v1, out var idx))
                 {
                     int[] result = {idx + 1, i + 1};
                     return result;
@@ -45,62 +80,61 @@ namespace LeetCode
             public void Test1()
             {
                 // Arrange
-                int[] numbers = {2,7,11,15};
-                int target = 9;
+                int[] numbers = {2, 7, 11, 15};
+                var target = 9;
 
                 // Act
-                int[] result = new Solution().TwoSum(numbers, target);
+                var result = new Solution().TwoSum(numbers, target);
 
                 // Assert
                 Assert.AreEqual(1, result[0]);
                 Assert.AreEqual(2, result[1]);
             }
-            
+
             [Test]
             public void Test2()
             {
                 // Arrange
-                int[] numbers = {2,3, 4};
-                int target = 6;
+                int[] numbers = {2, 3, 4};
+                var target = 6;
 
                 // Act
-                int[] result = new Solution().TwoSum(numbers, target);
+                var result = new Solution().TwoSum(numbers, target);
 
                 // Assert
                 Assert.AreEqual(1, result[0]);
                 Assert.AreEqual(3, result[1]);
             }
-            
+
             [Test]
             public void Test3()
             {
                 // Arrange
                 int[] numbers = {-1, 0};
-                int target = -1;
+                var target = -1;
 
                 // Act
-                int[] result = new Solution().TwoSum(numbers, target);
+                var result = new Solution().TwoSum(numbers, target);
 
                 // Assert
                 Assert.AreEqual(1, result[0]);
                 Assert.AreEqual(2, result[1]);
             }
-            
+
             [Test]
             public void Test4()
             {
                 // Arrange
                 int[] numbers = {0, 0, 3, 4};
-                int target = 0;
+                var target = 0;
 
                 // Act
-                int[] result = new Solution().TwoSum(numbers, target);
+                var result = new Solution().TwoSum(numbers, target);
 
                 // Assert
                 Assert.AreEqual(1, result[0]);
                 Assert.AreEqual(2, result[1]);
             }
-            
         }
     }
 }

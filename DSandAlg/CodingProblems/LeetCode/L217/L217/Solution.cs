4f460ccd94b2f67a566430d403ba03aa7a8e8d44
Sven <svenschmidt75@googlemail.com>
2021-05-05 16:17:45 -0600

    LeetCode 217: bitmap solution

--- DSandAlg/CodingProblems/LeetCode/L217/L217/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L217/L217/Solution.cs
@@ -14,9 +14,44 @@ namespace LeetCode
     {
         public bool ContainsDuplicate(int[] nums)
         {
+//            return ContainsDuplicate1(nums);
+            return ContainsDuplicate2(nums);
+        }
+
+        private bool ContainsDuplicate2(int[] nums)
+        {
+            // SS: runtime complexity: O(n)
+            // space complexity: O(1)
+
+            const long bitOffset = (int) 1E9;
+
+            var nBits = 2 * bitOffset + 1;
+            var bitmap = new byte[(nBits + 7) / 8];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var v = nums[i];
+
+                var byteBucket = (int) ((bitOffset + v) / 8);
+                var bit = (int) ((bitOffset + v) % 8);
+
+                // SS: number already seen?
+                if ((bitmap[byteBucket] & (1 << bit)) > 0)
+                {
+                    return true;
+                }
+
+                bitmap[byteBucket] |= (byte) (1 << bit);
+            }
+
+            return false;
+        }
+
+        private static bool ContainsDuplicate1(int[] nums)
+        {
             // SS: runtime complexity: O(n)
             // space complexity: O(n)
-            
+
             var dict = new HashSet<int>();
 
             for (var i = 0; i < nums.Length; i++)
@@ -35,43 +70,19 @@ namespace LeetCode
         [TestFixture]
         public class Tests
         {
-            [Test]
-            public void Test1()
+            [TestCase(new[] {1, 2, 3, 1}, true)]
+            [TestCase(new[] {1, 2, 3, 4}, false)]
+            [TestCase(new[] {1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true)]
+            [TestCase(new[] {(int) -1E9, 0, (int) 1E9}, false)]
+            public void Test(int[] nums, bool expected)
             {
                 // Arrange
-                int[] nums = {1, 2, 3, 1};
 
                 // Act
                 var result = new Solution().ContainsDuplicate(nums);
 
                 // Assert
-                Assert.True(result);
-            }
-
-            [Test]
-            public void Test2()
-            {
-                // Arrange
-                int[] nums = {1, 2, 3, 4};
-
-                // Act
-                var result = new Solution().ContainsDuplicate(nums);
-
-                // Assert
-                Assert.False(result);
-            }
-
-            [Test]
-            public void Test3()
-            {
-                // Arrange
-                int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
-
-                // Act
-                var result = new Solution().ContainsDuplicate(nums);
-
-                // Assert
-                Assert.True(result);
+                Assert.AreEqual(expected, result);
             }
         }
     }
---
+++ DSandAlg/CodingProblems/LeetCode/L303/L303/NumArray2.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class NumArray2
+    {
+        private readonly int[] _fennwickTree;
+
+        public NumArray(int[] nums)
+        {
+            _fennwickTree = new int[nums.Length + 1];
+
+            // SS: construct Fenwick tree
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var idx = i + 1;
+                var v = nums[i];
+
+                while (idx < _fennwickTree.Length)
+                {
+                    _fennwickTree[idx] += v;
+                    idx += Lsb(idx);
+                }
+            }
+        }
+
+        private int Lsb(int idx)
+        {
+            return idx & -idx;
+        }
+
+        private int Query(int idx)
+        {
+            var sum = 0;
+            while (idx > 0)
+            {
+                sum += _fennwickTree[idx];
+                idx -= Lsb(idx);
+            }
+
+            return sum;
+        }
+
+        public int SumRange(int left, int right)
+        {
+            var l = Query(left);
+            var r = Query(right + 1);
+            var d = r - l;
+            return d;
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void Test1()
+            {
+                // Arrange
+                var a = new NumArray(new[] {-2, 0, 3, -5, 2, -1});
+
+                // Act
+                // Assert
+                Assert.AreEqual(1, a.SumRange(0, 2));
+                Assert.AreEqual(-1, a.SumRange(2, 5));
+                Assert.AreEqual(-3, a.SumRange(0, 5));
+            }
+        }
+    }
+}

e733913fc766d04c9d95f2aa05e4d1599298ffd8
Sven <svenschmidt75@googlemail.com>
2021-05-08 14:30:54 -0600

    LeetCode 310

--- DSandAlg/CodingProblems/LeetCode/L310/L310/Solution.cs
+++ DSandAlg/CodingProblems/LeetCode/L310/L310/Solution.cs
@@ -1,7 +1,6 @@
 #region
 
 using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 
 #endregion
@@ -21,7 +20,10 @@ namespace LeetCode
 
         private IList<int> FindMinHeightTrees2(int n, int[][] edges)
         {
-            var result = new List<int>();
+            if (n == 1)
+            {
+                return new List<int> {0};
+            }
 
             // SS: create adj. list
             var adjList = new Dictionary<int, HashSet<int>>();
@@ -31,8 +33,8 @@ namespace LeetCode
             }
 
             // SS: indegree
-            int[] indegree = new int[n];
-            
+            var indegree = new int[n];
+
             for (var i = 0; i < edges.Length; i++)
             {
                 var edge = edges[i];
@@ -41,56 +43,53 @@ namespace LeetCode
 
                 indegree[n1]++;
                 indegree[n2]++;
-                
+
                 adjList[n1].Add(n2);
                 adjList[n2].Add(n1);
             }
 
             // SS: do BFS starting from the leaf nodes
-            var q = new Queue<int>();
-
-            var visited = new int[n];
-            int nLeaves = 0;
+            var leaves = new List<int>();
 
             for (var i = 0; i < n; i++)
             {
                 // SS: Outdegree and indegree are only defined for directed graphs, so we cannot
                 // use this here. Instead, a leaf node is one that has only one connection.
-                if (indegree[i] == 1)
+                if (adjList[i].Count == 1)
                 {
-                    visited[i] = 1;
-                    nLeaves++;
-                    q.Enqueue(i);
+                    leaves.Add(i);
                 }
             }
 
             // SS: peel away the outer leaves and add new leaves
-            while (nLeaves < n)
+            // number of non-leaf nodes left
+            var nodesLeft = n;
+            while (nodesLeft > 2)
             {
-                var leaf = q.Dequeue();
-                var neighbors = adjList[leaf];
-                foreach (var neighbor in neighbors)
+                // SS: every node is a leaf at some point, so subtract these/
+                // leaves from the total.
+                nodesLeft -= leaves.Count;
+
+                var newLeaves = new List<int>();
+
+                foreach (var leaf in leaves)
                 {
-                    if (visited[neighbor] == 1)
+                    var neighbors = adjList[leaf];
+                    foreach (var neighbor in neighbors)
                     {
-                        // SS: already processed
-                        continue;
-                    }
-                    
-                    indegree[neighbor]--;
-                    if (indegree[neighbor] == 1)
-                    {
-                        // SS: is now a leaf node
-                        q.Enqueue(neighbor);
-                        nLeaves++;
+                        // SS: remove edge (neighbor, leaf)
+                        indegree[neighbor]--;
+                        if (indegree[neighbor] == 1)
+                        {
+                            newLeaves.Add(neighbor);
+                        }
                     }
                 }
 
-                visited[leaf] = 1;
+                leaves = newLeaves;
             }
 
-            result = q.ToList();
-            return result;
+            return leaves;
         }
 
         private static IList<int> FindMinHeightTrees1(int n, int[][] edges)
@@ -245,7 +244,6 @@ namespace LeetCode
                 // Assert
                 CollectionAssert.AreEquivalent(new[] {0}, result);
             }
-            
         }
     }
 }

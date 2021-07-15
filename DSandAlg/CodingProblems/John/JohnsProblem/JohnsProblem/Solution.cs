1d8ab943c03d5b7904795a62f3722084b480e904
Sven Schmidt <s.schmidt@questintegrity.com>
2021-07-15 08:12:20 -0600

    solve John's problem

---
+++ DSandAlg/CodingProblems/John/JohnsProblem/JohnsProblem/Solution.cs
@@ -0,0 +1,97 @@
+﻿#region
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+#endregion
+
+namespace JohnsProblem
+{
+    public class Solution
+    {
+        public void Solve(double[] people)
+        {
+            // SS: two-pointer method
+            // runtime complexity: O(n)
+            // space complexity: O(1)
+            // assumptions: people[i] >= 0
+
+            // SS: calculate the average everybody should pay
+            double avg = 0;
+            for (int p = 0; p < people.Length; p++)
+            {
+                avg += people[p];
+            }
+            avg /= people.Length;
+
+            // SS: two-pointer method
+            // SS: pointer i points to people that payed less than the average
+            // SS: pointer j points to people that payed more than the average
+            int i = 0;
+            int j = 0;
+
+            while (i < people.Length)
+            {
+                while (i < people.Length && people[i] >= avg)
+                {
+                    i++;
+                }
+
+                if (i == people.Length)
+                {
+                    // SS: we are done
+                    break;
+                }
+
+                while (j < people.Length && people[j] <= avg)
+                {
+                    j++;
+                }
+
+                if (j == people.Length)
+                {
+                    // SS: we are done
+                    break;
+                }
+
+                double iAmount = people[i];
+                double jAmount = people[j];
+
+                // SS: amount i should pay
+                double iGiveAmount = avg - iAmount;
+
+                // SS: amount j should receive
+                // cannot exceed what i should pay
+                double jReceiveAmount = Math.Min(jAmount - avg, iGiveAmount);
+
+                // SS: i writes j a check
+                people[i] += jReceiveAmount;
+                people[j] -= jReceiveAmount;
+            }
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            [TestCase(new[] {5.0, 7, 13, 2, 15})]
+            [TestCase(new[] { 5.0, 5, 5, 5, 5 })]
+            [TestCase(new[] { 5.0 })]
+            public void Test(double[] people)
+            {
+                // Arrange
+
+                // Act
+                new Solution().Solve(people);
+
+                // Assert
+                double avg = people.Sum() / people.Length;
+                for (int i = 0; i < people.Length; i++)
+                {
+                    Assert.That(people[i], Is.EqualTo(avg).Within(1E-8));
+                }
+            }
+
+        }
+    }
+}
---
+++ DSandAlg/CodingProblems/John/JohnsProblem/JohnsProblem/Solution2.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using LeetCode;
+using NUnit.Framework;
+
+namespace JohnsProblem
+{
+    public class Solution2
+    {
+        public int Solve(double[] people)
+        {
+            // SS: runtime complexity: O(n log n)
+            // space complexity: O(n)
+
+            // SS: calculate the average everybody should pay
+            double avg = 0;
+            for (int p = 0; p < people.Length; p++)
+            {
+                avg += people[p];
+            }
+            avg /= people.Length;
+
+            // SS: O(n log n)
+            var payerHeap = BinaryHeap<double>.CreateMaxHeap();
+            var payeeHeap = BinaryHeap<double>.CreateMaxHeap();
+            for (int p = 0; p < people.Length; p++)
+            {
+                double d = avg - people[p];
+                if (d < 0)
+                {
+                    // SS: this person payed more than the avg, so it will receive money
+                    payeeHeap.Push(-d);
+                }
+                else if (d > 0)
+                {
+                    // SS: this person payed less than the avg, so it will pay money
+                    payerHeap.Push(d);
+                }
+
+                // SS: no need to push on heap if people[p] == avg
+            }
+
+            int nChecks = 0;
+
+            // SS: runtime complexity of loop: depends on the specifics of the values,
+            // but there is no dependency on the size of the input array, so O(n log n)
+            // avg.
+
+            // SS: find the person who owes the most. Then, pay the person who is owed
+            // the most, etc. If there is money left over, find the next payee, as the
+            // money needs to be spent no matter what.
+            while (payeeHeap.IsEmpty == false)
+            {
+                double amountToSpend = payerHeap.Pop();
+                while (Math.Abs(amountToSpend) > 1E-3)
+                {
+                    nChecks++;
+
+                    double amountToReceive = payeeHeap.Pop();
+                    if (amountToSpend - amountToReceive < 0)
+                    {
+                        // SS: payee cannot be payed completely
+                        payeeHeap.Push(amountToReceive - amountToSpend);
+                    }
+
+                    amountToSpend = Math.Max(0, amountToSpend - amountToReceive);
+                }
+            }
+
+            return nChecks;
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            [TestCase(new[] { 5.0, 7, 13, 2, 15 }, 4)]
+            [TestCase(new[] { 5.0, 5, 5, 5, 5 }, 0)]
+            [TestCase(new[] { 5.0 }, 0)]
+            public void Test(double[] people, int expectedResult)
+            {
+                // Arrange
+
+                // Act
+                int nChecks = new Solution2().Solve(people);
+
+                // Assert
+                Assert.AreEqual(nChecks, expectedResult);
+            }
+
+        }
+    }
+}

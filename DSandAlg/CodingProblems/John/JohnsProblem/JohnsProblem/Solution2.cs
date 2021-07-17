9aaccb45c0e9549ffcfe1fca27a68b16b40e1e44
Sven <svenschmidt75@googlemail.com>
2021-07-17 08:59:21 -0600

    write mi number of checks, brute-force

--- DSandAlg/CodingProblems/John/JohnsProblem/JohnsProblem/Solution2.cs
+++ DSandAlg/CodingProblems/John/JohnsProblem/JohnsProblem/Solution2.cs
@@ -9,6 +9,119 @@ namespace JohnsProblem
     {
         public int Solve(double[] people)
         {
+            // return SolveGreedily(people);
+            return SolveBruteForce(people);
+        }
+
+        private int SolveBruteForce(double[] people)
+        {
+            // SS: brute-force solution, i.e. calculate all solutions
+            // and filter (i.e. take the one with the min number of
+            // checks)
+            // runtime complexity: O(n!)
+
+            // SS: shift the average to be 0
+            double avg = 0;
+            for (int i = 0; i < people.Length; i++)
+            {
+                avg += people[i];
+            }
+            avg /= people.Length;
+            
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] -= avg;
+                
+                // SS: when close to 0...
+                if (Math.Abs(people[i]) < 1E-10)
+                {
+                    people[i] = 0;
+                }
+            }
+            
+            int nZeros = 0;
+            
+            int Solve(int state, int idx)
+            {
+                // SS: base case
+                if (nZeros == people.Length)
+                {
+                    return 0;
+                }
+
+                int minChecks = int.MaxValue;
+
+                if (state == 0)
+                {
+                    // SS: find new unmarked
+                    for (int i = 0; i < people.Length; i++)
+                    {
+                        if (people[i] == 0)
+                        {
+                            continue;
+                        }
+
+                        int nChecks = Solve(1, i);
+                        minChecks = Math.Min(minChecks, nChecks);
+                    }
+                }
+                else
+                {
+                    // SS: 
+                    for (int i = 0; i < people.Length; i++)
+                    {
+                        if (people[i] == 0)
+                        {
+                            continue;
+                        }
+
+                        // SS: set one to the avg, the other one to the difference
+                        double v1 = people[idx];
+                        double v2 = people[i];
+                        
+                        // SS: both must have opposite signs, otherwise they cannot (partially)
+                        // cancel ech other
+                        if (v1 == v2 || v1 * v2 > 0)
+                        {
+                            continue;
+                        }
+                        
+                        // SS: avg is 0
+                        people[idx] = 0;
+                        people[i] = v1 + v2;
+
+                        int zeroCount = 1;
+                        if (Math.Abs(v1 + v2) < 1E-10)
+                        {
+                            people[i] = 0;
+                            zeroCount++;
+                        }
+
+                        nZeros += zeroCount;
+                        int nChecks = Solve(0, 0);
+                        if (nChecks < int.MaxValue)
+                        {
+                            nChecks++;
+                        }
+
+                        // SS: backtracking
+                        nZeros -= zeroCount;
+                        people[i] = v2;
+                        people[idx] = v1;
+
+                        minChecks = Math.Min(minChecks, nChecks);
+                    }
+                }
+
+                return minChecks;
+            }
+
+            int minChecks = Solve(0, 0);            
+            return minChecks < int.MaxValue ? minChecks : 0;
+        }
+
+        private static int SolveGreedily(double[] people)
+        {
             // SS: runtime complexity: O(n log n)
             // space complexity: O(n)
 
@@ -18,6 +131,7 @@ namespace JohnsProblem
             {
                 avg += people[p];
             }
+
             avg /= people.Length;
 
             // SS: O(n log n)
@@ -74,6 +188,8 @@ namespace JohnsProblem
         public class Tests
         {
             [TestCase(new[] { 5.0, 7, 13, 2, 15 }, 4)]
+            [TestCase(new[] { 5.0, 3, 1, -3, -6 }, 3)]
+            [TestCase(new[] { -6.0, -2, -2, 3, 3, 4 }, 4)]
             [TestCase(new[] { 5.0, 5, 5, 5, 5 }, 0)]
             [TestCase(new[] { 5.0 }, 0)]
             public void Test(double[] people, int expectedResult)
@@ -84,7 +200,7 @@ namespace JohnsProblem
                 int nChecks = new Solution2().Solve(people);
 
                 // Assert
-                Assert.AreEqual(nChecks, expectedResult);
+                Assert.AreEqual(expectedResult, nChecks);
             }
 
         }

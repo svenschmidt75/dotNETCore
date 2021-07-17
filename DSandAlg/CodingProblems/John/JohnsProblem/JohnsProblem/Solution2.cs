df4745981271bc4bb803758dd7792ca4ed695a08
Sven <svenschmidt75@googlemail.com>
2021-07-17 11:04:08 -0600

    potentially a faster O(n^3) solution

--- DSandAlg/CodingProblems/John/JohnsProblem/JohnsProblem/Solution2.cs
+++ DSandAlg/CodingProblems/John/JohnsProblem/JohnsProblem/Solution2.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using LeetCode;
 using NUnit.Framework;
 
@@ -7,10 +8,217 @@ namespace JohnsProblem
 {
     public class Solution2
     {
+        private int SolveGreedily2(double[] people)
+        {
+            // SS: runtime complexity: O(n^3)
+            // Idea: find pairs (v1, v2) where v1 < - and v2 > 0, and prioritize:
+            // best: v1 = -v2
+            // 2nd best: v1 + v2 = v3 and v3 is contained in the list
+            // else
+            // We then transfer values != 0 over to a tmp. array
+            // and solve the same problem, but smaller.
+            
+            // SS: shift the average to be 0
+            double avg = 0;
+            for (int i = 0; i < people.Length; i++)
+            {
+                avg += people[i];
+            }
+            avg /= people.Length;
+
+            var dict = new Dictionary<double, int>();
+            
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] -= avg;
+                
+                // SS: when close to 0...
+                if (Math.Abs(people[i]) < 1E-10)
+                {
+                    people[i] = 0;
+                }
+
+                if (dict.ContainsKey(people[i]))
+                {
+                    dict[people[i]]++;
+                }
+                else
+                {
+                    dict[people[i]] = 1;
+                }
+            }
+
+            // SS: sort in increasing order
+            Array.Sort(people);
+
+            if (people[0] >= 0)
+            {
+                return 0;
+            }
+            
+            int i3 = 0;
+            while (i3 < people.Length && people[i3] < 0)
+            {
+                i3++;
+            }
+            
+            int nChecks = 0;
+            
+            while (people.Length > 0)
+            {
+                bool foundZero  = false;
+                bool foundOpposite = false;
+                int fo1 = -1;
+                int fo2 = -1;
+                
+                int i1 = 0;
+                while (people[i1] < 0 && i1 < i3)
+                {
+                    double v1 = people[i1];
+
+                    int i2 = i3;
+                    while (i2 < people.Length)
+                    {
+                        double v2 = people[i2];
+                        double delta = v1 + v2;
+
+                        if (delta == 0)
+                        {
+                            // SS: cancel, best case
+                            dict[people[i1]]--;
+                            if (dict[people[i1]] == 0)
+                            {
+                                dict.Remove(people[i1]);
+                            }
+                            people[i1] = 0;
+
+                            dict[people[i2]]--;
+                            if (dict[people[i2]] == 0)
+                            {
+                                dict.Remove(people[i2]);
+                            }
+                            people[i2] = 0;
+
+                            foundZero = true;
+
+                            nChecks++;
+                            
+                            break;
+                        }
+                        
+                        if (dict.ContainsKey(-delta))
+                        {
+                            // SS: we have the opposite value in the list,
+                            // next best case
+                            foundOpposite = true;
+                            fo1 = i1;
+                            fo2 = i2;
+                        }
+
+                        if (foundOpposite == false)
+                        {
+                            // SS: take any pair
+                            fo1 = i1;
+                            fo2 = i2;
+                        }
+                        
+                        i2++;
+                    }
+
+                    if (foundZero)
+                    {
+                        fo1 = -1;
+                        fo2 = -1;
+                        break;
+                    }
+
+                    i1++;
+                }
+
+                double value = double.MaxValue;
+
+                if (foundZero == false)
+                {
+                    value = people[fo1] + people[fo2];
+
+                    dict[people[fo1]]--;
+                    if (dict[people[fo1]] == 0)
+                    {
+                        dict.Remove(people[fo1]);
+                    }
+                    people[fo1] = 0;
+
+                    dict[people[fo2]]--;
+                    if (dict[people[fo2]] == 0)
+                    {
+                        dict.Remove(people[fo2]);
+                    }
+                    people[fo2] = value;
+                
+                    if (dict.ContainsKey(value))
+                    {
+                        dict[value]++;
+                    }
+                    else
+                    {
+                        dict[value] = 1;
+                    }
+                
+                    nChecks++;
+                }
+
+                // SS: copy values over
+                int count = people.Length - 1;
+                if (foundZero)
+                {
+                    count--;
+                }
+
+                i3 = -1;
+                
+                double[] people2 = new double[count];
+                int p2 = 0;
+                for (int i = 0; i < people.Length; i++)
+                {
+                    if (people[i] == 0 || i == fo2)
+                    {
+                        continue;                        
+                    }
+                    
+                    if (value < people[i])
+                    {
+                        people2[p2] = value;
+                        value = double.MaxValue;
+
+                        if (i3 < 0 && people2[p2] > 0)
+                        {
+                            i3 = p2;
+                        }
+
+                        p2++;
+                    }
+
+                    people2[p2] = people[i];
+
+                    if (i3 < 0 && people2[p2] > 0)
+                    {
+                        i3 = p2;
+                    }
+
+                    p2++;
+                }
+
+                people = people2;
+            }
+
+            return nChecks;
+        }
+
         public int Solve(double[] people)
         {
             // return SolveGreedily(people);
-            return SolveBruteForce(people);
+//            return SolveBruteForce(people);
+            return SolveGreedily2(people);
         }
 
         private int SolveBruteForce(double[] people)
